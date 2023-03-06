1e0a89a3e6d5f08c2963fccfd59b2c41ad62c4b7
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-03-05 21:16:56 -0300

    removing curve fit func

--- utils.py
+++ utils.py
@@ -1,28 +1,8 @@
-from scipy.optimize import curve_fit
 from sklearn.metrics import r2_score
-import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, r2_score
 
 
-
-def func(x, a, c):
-    return a * x + c
-
-
-def get_fit(xdata, ydata):
-    
-    popt, pcov = curve_fit(func, xdata, ydata)
-
-    y_pred = func(xdata, *popt)
-
-    r2 = round(r2_score(ydata, y_pred), 2)
-    
-    return func(xdata, *popt), r2 
-
-
-def get_fit2(x, y):
+def get_fit(x, y):
     
     regression_model = LinearRegression()
     # Fit the data(train the model)
@@ -30,8 +10,6 @@ def get_fit2(x, y):
     # Predict
     y_predicted = regression_model.predict(x)
 
-    # model evaluation
-    rmse = mean_squared_error(y, y_predicted)
     r2 = r2_score(y, y_predicted)
     return round(r2, 2), y_predicted
 
