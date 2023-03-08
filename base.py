3f5c967deac1d63fb3d213b7573b5106a6220b41
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-03-08 13:24:57 -0300

    adding find_sames func

--- base.py
+++ base.py
@@ -1,6 +1,9 @@
 import os
 import datetime as dt
 from liken.plotting.plot_nighttime_corr_FPI_EPB import plot_nigthttime_corr_FPI_EPB
+import matplotlib.pyplot as plt
+import pandas as pd
+
 
 def get_datetime_fpi(filename):
     s = filename.split('_')
@@ -14,31 +17,74 @@ def get_datetime_epb(filename):
     month = int(mon_day[:2])
     day = int(mon_day[2:])
     year = int(year)
+    
     if lat == "":
         lat = 0
     else:
-     lat = int(lat)
+        lat = int(lat)
     return dt.datetime(year, month, day), lat
 
-infile_epb = "database/EPBs/"
-infile_fpi = "database/FabryPerot/2013/"
 
-for f1 in os.listdir(infile_epb ):
-    date_epb, lat = get_datetime_epb(f1)
 
-    for f2 in os.listdir(infile_fpi):
+def save_img(fig, 
+             save_in):
+    
+    plt.ioff()
+    fig.savefig(save_in, 
+                dpi = 100, 
+                pad_inches = 0, 
+                bbox_inches = "tight")
+    plt.clf()   
+    plt.close()
+    return 
 
-        date_fpi = get_datetime_fpi(f2)
+
+
+infile_epb = "database/EPBs/drift/"
+infile_fpi = "database/FabryPerot/2012/"
+
+
+
+def find_sames(lat_check = 5, 
+               threshold = 0.6):
+    out = []
+    for f1 in os.listdir(infile_epb):
+        date_epb, lat = get_datetime_epb(f1)
+    
+        for f2 in os.listdir(infile_fpi):
+    
+            date_fpi = get_datetime_fpi(f2)
+                    
+            if (date_fpi == date_epb) and (lat == lat_check):
+                FPI_file = os.path.join(infile_fpi, f2)
+                EPB_file = os.path.join(infile_epb, f1)
+                try:
+                    FigureName =  f1.replace("txt", "png")
+                    save_in = os.path.join(
+                        "C:\\plots",
+                        FigureName
+                        )
+                    fig, r2, df = plot_nigthttime_corr_FPI_EPB(
+                        FPI_file, 
+                        EPB_file,
+                        lat
+                        )
+                    
+                    save_img(fig, save_in)
+                    
+                    if r2 > threshold:
+                        out.append(df)
+    
+                except:
+                    continue      
                 
-        if (date_fpi == date_epb) and (lat == 5):
-            FPI_file = os.path.join(infile_fpi, f2)
-            EPB_file = os.path.join(infile_epb, f1)
-            try:
-                fig = plot_nigthttime_corr_FPI_EPB(
-                    FPI_file, EPB_file
-                    )
-            except:
-                continue      
+    
+    ts = pd.concat(out)
+    
+    ts.to_csv("corr_06.txt", index = True)
+    return ts
 
 
+ts = find_sames(lat_check = 5)
+print(ts)
        
