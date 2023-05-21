399d84d2dab260c8b5c76e6619813e958ac80be4
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-05-20 22:19:18 -0300

    removing extra funcs

--- src/plotting/plot_all_observations.py
+++ src/plotting/plot_all_observations.py
@@ -5,19 +5,11 @@ import cv2
 import matplotlib.gridspec as gridspec
 import pandas as pd
 import locale
-
-
-from GNSS.plotConfig import mapping
 from plotTECmaps import *
 
 import cartopy.crs as ccrs
 
-from imager.image_settings import load_and_processing
-from imager.image_labeling import draw_labels
 
-from results.results_utils import ordering, date_from_filename
-
-from results.plot.plotROTImaximus import *
 
 
 def get_files(infile, extension = ""): 
@@ -47,113 +39,6 @@ def crop_image(file, y = 50, x = 130, h = 900, w = 750):
 
 
 
-def plot_quadri_observation(date, 
-                            infile,
-                            imagerFilename, 
-                            ionoFilename, 
-                            tecFilename, 
-                            save = False, 
-                            path_to_save = "C:\\tri_observation", 
-                            num_time = 0):
-    
-    if save:
-        plt.ioff()
-        
-        
-    delta = datetime.timedelta(days = 1)
- 
-    times = pd.date_range(f"{date} 21:00", 
-                          f"{date + delta} 07:00", 
-                          freq = "10min")
-    
-    
-        
-    fig = plt.figure(figsize = (22, 10))
-    
-    plt.subplots_adjust(wspace = 0.2, hspace = 0.5)
-    
-    G = gridspec.GridSpec(3, 3)
-    
-    
-    ax1 = plt.subplot(G[0, :])
-    
-    df = load_and_plot_roti(ax1, infile_from_date(date), 
-                            times[0], times[-1])
-    
-    
-    
-    shade(ax1, df, date_from_filename(tecFilename).datetime, 
-          threshold = 1)
-    
-    
-    ax2 = plt.subplot(G[1:, -1], projection = ccrs.PlateCarree())
-    
-    plotTECmap(ax2, os.path.join(infile, "tec\\"), tecFilename)
-    
-    
-    ax3 = plt.subplot(G[1:, 0])
-    
-    
-    if imagerFilename == None:
-    
-        ax3.text(0.15, 0.5, "Sem imagens", transform = ax3.transData, 
-                 color = "red", fontsize = 40)
-        
-        plot_image(None, ax3)
-        name = date_from_filename(tecFilename).datetime
-        name += datetime.timedelta(minutes = num_time)
-        
-    else:
-        img = load_and_processing(os.path.join(infile, "imager", 
-                                                    imagerFilename))
-        plot_image(img, ax3)
-    
-        draw_labels(ax3, imagerFilename, fontsize = 18, 
-                    color = "white")
-        
-        name = date_from_filename(imagerFilename).datetime
-    
-    ax4 = plt.subplot(G[1:, -2])
-        
-    plot_image(crop_image(os.path.join(infile, 
-                                       f"ionosonde\\{ionoFilename}")), ax4)
-    
-    
-    ax4.set(ylabel = "Altura (km)", xlabel = "Frequência (Hz)", 
-            title =  date_from_filename(tecFilename).datetime)
-    
-    x = 0.3
-    y = -0.15
-    
-    labels = ["Mapa de TEC (Brasil)", 
-              "Imageador (Cariri)", 
-              "Digissonda (Fortaleza)"]
-    
-    
-    for num, ax in enumerate([ax2, ax3, ax4]):
-    
-        ax.text(x, y, labels[num], transform = ax.transAxes)            
-  
-    
-    locale.setlocale(locale.LC_ALL, 'pt_pt.UTF-8')
-    day1 = date.strftime('%d')
-    day2 = (date + delta).strftime('%d')
-    fig.suptitle(f"Observações: {day1}-{day2} de {date.strftime('%B')} de {date.year}", 
-                 y = 0.94)
-    
-    
-    if save:
-        print("Saving...", name)
-        name_to_save = name.strftime("%Y%m%d%H%M%S") 
-        fig.savefig(f"{path_to_save}\\{name_to_save}.png", 
-                   dpi = 100, bbox_inches="tight")
-        
-        plt.clf()   
-        plt.close()
-    else:
-        plt.show()
-        
-        
 
 
  
@@ -192,71 +77,16 @@ def run_for_all_day(date, image_files, tec_files,
                              path_to_save = path_to_save)       
             
             
-#run_for_all_day(date, image_files, tec_files, iono_files, infile)
-
-def make_dir(infile):
-    """Create a new directory by path must be there year and doy"""
+def layout():
+    fig = plt.figure(figsize = (22, 10))
     
-    path_to_create = os.path.join(infile, "joined")
+    plt.subplots_adjust(wspace = 0.2, hspace = 0.5)
+    
+    G = gridspec.GridSpec(3, 3)
     
     
-    try:
-        os.mkdir(path_to_create)
-        
-        print(f"Creation of the directory {path_to_create} successfully")
-        
-    except OSError:
-        print(f"Creation of the directory {path_to_create} failed")
-      
-   
-    return path_to_create
+    ax1 = plt.subplot(G[0, :])
+    ax2 = plt.subplot(G[1:, -1], projection = ccrs.PlateCarree())
+    ax3 = plt.subplot(G[1:, 0])
 
-
-path_root = "C:\\Users\\Public\\observation\\"
-
-_, folders, _  = next(os.walk(path_root))
-
-year = 2014
-for folder in folders:
-    
-    if folder == "225":
-    
-        date = datetime.date(year, 1, 1) + datetime.timedelta(int(folder) - 1)
-        
-        
-        infile = os.path.join(path_root, folder)
-        
-        
-        image_files = get_files(os.path.join(infile, "imager"))
-        
-        
-        tec_files = get_files(os.path.join(infile, "tec"))
-        
-        
-        iono_files = get_files(os.path.join(infile,"ionosonde"), 
-                           extension = ".PNG")
-        
-        path_to_save = make_dir(infile)
-        
-        try:
-            run_for_all_day(date, image_files, tec_files, 
-                        iono_files, infile, path_to_save)
-            
-        except:
-            print(folder, "não deu")
-            continue
-#
-"""
-out = ordering(date, image_files)
-num = 10
-tecFilename, ionoFilename, imagerFilename = tuple(out[num])
-
-plot_quadri_observation(date, 
-                 infile,  
-                 imagerFilename, 
-                 ionoFilename, 
-                 tecFilename, 
-                 save = False, 
-                 path_to_save = path_to_save,
-                 num_time = 0)
-"""
+    ax4 = plt.subplot(G[1:, -2])
---
+++ src/plotting/plot_collision_frequencies.py
@@ -0,0 +1,37 @@
+import FluxTube as ft
+import ionosphere as io
+from GEO import load_meridian
+import datetime as dt
+import matplotlib.pyplot as plt
+dn = dt.datetime(2013, 1, 1, 21, 0) 
+
+mlon, mlat, _, _, = load_meridian()
+
+kwargs = dict(
+    dn = dn, 
+    glat = mlat, 
+    glon = mlon,
+    hmin = 90,
+    step = 5,
+    hmax = 1000
+    
+    )
+
+ds = io.test_data(**kwargs)
+
+
+def plot_collision_frequency(ax):
+
+   
+    #ax.plot(nu_eff, apex)
+    
+    ax.set(xlabel = "$\nu_{eff}^F$", 
+           ylabel = "Altura de apex (km)")
+
+
+infile = "database/FluxTube/201301012100.txt"
+
+ds = io.load_calculate(infile)
+ne = ft.IntegratedParameters(ds)
+
+ds
