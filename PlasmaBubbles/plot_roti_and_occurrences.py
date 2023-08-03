e76aa28f027440cc84086a3192a86fab566c4211
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-08-03 04:46:51 -0300

    updates

--- PlasmaBubbles/plot_roti_and_occurrences.py
+++ PlasmaBubbles/plot_roti_and_occurrences.py
@@ -1,16 +1,10 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jul 31 18:39:18 2023
+import matplotlib.pyplot as plt
+from base import format_time_axes
+import datetime as dt
+from base import load
 
-@author: Luiz
-"""
 
 def plot_roti_and_occurrences(df, date):
-    
-
-    import matplotlib.pyplot as plt
-    from base import format_time_axes
-    import datetime as dt
     
     fig, ax = plt.subplots(
         figsize = (8, 6), 
@@ -43,3 +37,48 @@ def plot_roti_and_occurrences(df, date):
             color = 'blue',
             lw = 2
             )
+
+import numpy as np
+import pandas as pd
+
+dn = dt.datetime(2013, 1, 4, 12)
+ds = dt.datetime(2013, 1, 2, 12)
+
+df = load('2013.txt')
+df = df.loc[(df.index > ds) &
+            (df.index < dn)]
+
+fig, ax = plt.subplots(
+    figsize = (12, 8), 
+    nrows = 2, 
+    sharex= True,
+    dpi = 300
+    )
+plt.subplots_adjust(hspace=0.1)
+def plot_roti(ax, df):
+    ax.plot(df['ceeu'])
+    
+    
+    dates = np.unique(df.index.date)
+    
+    for dn in pd.to_datetime(dates):
+        delta = dt.timedelta(hours = 3)
+        
+        ax.axvline(dn + delta, color = 'red', lw = 2)
+        
+    ax.set(ylabel = 'ROTI (TECU/min)', 
+           xlim = [df.index[0], df.index[-1]])
+    
+    ax.axhline(1, color = 'magenta', lw = 2)
+    
+
+plot_roti(ax[0], df)
+import PlasmaBubbles as pb
+
+ds = pb.check_occurrence_in_all(df)
+
+ax[1].plot(ds['same'])
+
+format_time_axes(ax[1], hour_locator = 4)
+
+ax[1].set(ylabel = 'EPB occurrence')
