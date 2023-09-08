02bac7bf361d2cce2fa3ca192c05bc9f948e18b7
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-09-08 05:41:31 -0300

    new plotting

---
+++ distributions/plot_distributions_seasons.py
@@ -0,0 +1,80 @@
+import base as b 
+import matplotlib.pyplot as plt 
+import events as ev
+from plotting import  plot_distribution
+
+def plot_distributions_seasons():
+    
+
+    fig, ax = plt.subplots(
+        ncols = 2,
+        nrows = 2, 
+        figsize = (15, 8), 
+        sharex = True, 
+        sharey = True, 
+        dpi = 300
+        )
+    
+    ks = {
+         
+         3: 'march equinox',
+         6: 'june solstice',
+         9: 'setember equinox',
+         12: 'december solstice'
+        
+         }
+    
+    plt.subplots_adjust(wspace = 0.1)
+    
+    df = b.load('all_results.txt')
+    
+    df['doy'] = df.index.day_of_year
+    
+    def kp_levels(df):
+        quiet = df[df['kp_max'] <= 3]
+        
+        disturbed = df[df['kp_max'] > 3]
+        
+        return [quiet, disturbed]
+    
+    for i, ax in enumerate(ax.flat):
+        
+        month = (i + 1) * 3
+        season_name = ks[month]
+        
+        count = []
+        name = ['$Kp \\leq 3$', '$Kp > 3$']
+        
+        for i, level in enumerate(kp_levels(df)):
+            index = i + 1
+            ds = ev.seasons(level, month)
+        
+            c = plot_distribution(
+                    ax, 
+                    ds, 
+                    f'({index}) {name[i]}'
+                    )
+            
+            count.append(f'({index}) {c} events')
+        
+        infos = 'EPB occurrence\n' + '\n'.join(count)
+            
+        ax.text(0.65, 0.25, infos, transform = ax.transAxes)
+        
+        ax.set(title = season_name.title())
+    
+    
+    ax.legend(ncol = 2, 
+              bbox_to_anchor = (-.1, 2.6),
+              loc = "upper center")
+    
+    fontsize = 30
+    fig.text(0.05, 0.25, 
+             "EPB occurrence probability", 
+             fontsize = fontsize, rotation = 'vertical')
+    
+    fig.text(0.4, 0.05, 
+             "$\\gamma_{FT}~$ ($\\times 10^{-3}~s^{-1}$)", 
+             fontsize = fontsize)
+    
+plot_distributions_seasons()
