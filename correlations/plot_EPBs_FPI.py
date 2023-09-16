21b8429640eb2cddd74daa363c9b74da809d28b2
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-09-16 13:25:06 -0300

    new plot

---
+++ correlations/plot_EPBs_FPI.py
@@ -0,0 +1,52 @@
+import matplotlib.pyplot as plt
+import base as b
+import pandas as pd 
+def plot_corr_EPB_FPI(fontsize = 20):
+    
+    df = b.load('roti_evo.txt')
+
+    ip = b.load('database/indices/indeces.txt')
+
+
+    ip
+
+    df = df.loc[df['lon'] == -40]
+
+    # df['mean'].plot()
+
+    # df['base'].plot()
+
+    # ip = b.load('database/indices/solar_flux.txt')
+
+    # import numpy as np
+
+    # # ip = ip.replace((-1, 0), np.nan)
+    ds = pd.concat([df, ip], axis = 1).dropna()
+
+    ds = ds.loc[(ds['base'] > 0.1) & 
+                (ds['mean'] < 3)]
+      
+    import matplotlib.pyplot as plt 
+
+
+    fig, ax = plt.subplots(
+        dpi = 300, 
+        sharex = True, 
+        figsize = (10, 4)
+        )
+
+    x, y = ds['f107'].values / 100, ds['mean'].values
+
+    ax.scatter(x, y)
+
+    r2, ypred = b.linear_fit(x, y)
+
+    ax.plot(x, ypred, lw = 2, color = 'r', label = r2)
+
+    ax.set(xlim = [0, 3], ylim = [0, 3])
+    ax.legend()
+
+    
+
+    return fig
+
