904191deb2ccdabe878712936d6dee30f3b2d422
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-10-14 18:14:23 -0300

    new layout

--- climatologies/plot_pre_annual.py
+++ climatologies/plot_pre_annual.py
@@ -1,53 +1,53 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-import base as s
+import base as b
 
-def dataset(site = 'saa', ys = 2013, ye = 2023):
-    
-    infile = f'digisonde/data/drift/PRE/{site}/'
+b.config_labels()   
 
-    years = list( range(ys, ye))
+def set_data(site = 'saa'):
+
+    infile = f'digisonde/data/PRE/{site}/2013_2021.txt'
+    
+    df = b.load(infile)
+    
+    df.rename(
+        columns = {'vp': 'vz'}, 
+        inplace = True
+        )
+    
+    return df 
+
+def plot_pre_annual_site(ax, df):
     
     
-    out = []
-    for year in years:
-        ds = s.load(infile + f'{year}.txt')
-        
-        
+       
+    ax.scatter(df.index, df['vz'])
     
-   
-    return pd.concat(out)
+    ax.set(
+        ylabel = "Vertical drift (m/s)", 
+        xlim = [df.index[0], df.index[-1]], 
+        ylim = [-10, 80]
+        )
+    
     
 
-def plot_pre_annual_variation():
+
+
+def plot_pre_annual():
     
     fig, ax = plt.subplots(
         dpi = 300,
-        figsize = (10, 6)
+        nrows = 2,
+        sharex = True,
+        sharey = True,
+        figsize = (14, 8)
         )
-       
-    df = dataset()
-    ax.scatter(df.index, df['vp'])
+    
+    df = set_data(site = 'saa')
+    plot_pre_annual_site(ax[0], df)
+    
+    df = set_data(site = 'jic')
+    plot_pre_annual_site(ax[1], df)
     
     
-    ax.set(ylabel = "Vertical drift (m/s)", 
-           xlim = [df.index[0], df.index[-1]], 
-           ylim = [-10, 80])
-    
-    s.axes_month_format(
-            ax, 
-            month_locator = 4, 
-            pad = 50)
-    
-    plt.show()
-    
-    return fig
-
-# f = plot_pre_annual_variation()
-
-df = dataset()
-
-df
-
-    
-    
+    ax[1].set(xlabel = 'years')
