fccd0a20b3f458425cb5ed764ce3523916007ad2
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-10-19 17:50:27 -0300

    annual variation

--- bars/plot_events_with_solar_flux.py
+++ bars/plot_events_with_solar_flux.py
@@ -13,13 +13,14 @@ def plot_epbs_by_solar_cycle():
     fig, ax = plt.subplots(
         nrows = 2, 
         dpi = 300, 
-        figsize = (12, 8), 
-        sharey = True
+        figsize = (12, 8)
         )
+    
+    plt.subplots_adjust(hspace = 0.1)
      
     df = b.load(path)
     
-    pb.year_occurrence(df, 1).plot(
+    pb.year_occurrence(df, 1)['-50'].plot(
         kind = 'bar', 
         ax = ax[0], 
         legend = False
@@ -32,19 +33,25 @@ def plot_epbs_by_solar_cycle():
     ax[1].plot(ds['f107'])
     ax[1].plot(ds['f107a'], lw = 2)
     
-    ax[0].set( ylabel = 'Nigths with EPB')
-    ax[1].set(xlabel = 'Years', ylabel = '$F_{10.7}$ sfu')
-    ax[1].axhline(110, color = 'r', lw = 2)
-    fig.autofmt_xdate(rotation=0)
-
-    ax[0].legend(
-        ncols = 5, 
-        bbox_to_anchor = (.5, 1.5), 
-        loc = "upper center", 
-        title = 'longitudinal sectors'
+    ax[0].set(
+        ylabel = 'Nigths with EPB', 
+        title = 'Annually EPBs occcurrence'
         )
     
-    return 
+    ax[1].set(xlabel = 'Years', ylabel = '$F_{10.7}$ sfu')
+    ax[1].axhline(100, color = 'r', lw = 2)
+    fig.autofmt_xdate(rotation=0)
+
+    # ax[0].legend(
+    #     ['EPB events occurrence'],
+    #     ncols = 5, 
+    #     bbox_to_anchor = (.5, 1.5), 
+    #     loc = "upper center"
+    #     )
+    
+    return fig
 
 
-plot_epbs_by_solar_cycle()
+# fig = plot_epbs_by_solar_cycle()
+
+# fig.savefig(b.LATEX + 'paper1/annual_variation')
