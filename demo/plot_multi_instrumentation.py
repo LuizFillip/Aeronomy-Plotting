1f48cae7ec5b4cd614afbdb79beb9e37b297d168
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-10-25 13:20:43 -0300

    updates

--- demo/plot_multi_instrumentation.py
+++ demo/plot_multi_instrumentation.py
@@ -8,9 +8,7 @@ import imager as im
 import base as b 
 import datetime as dt 
 
-def plot_imager(fname, row, col):
-    
-    ax2 = fig.add_subplot(gs[row, col])
+def plot_imager(ax2, fname):
     
     img = io.imread(fname, as_gray = True)
     
@@ -28,12 +26,14 @@ def plot_imager(fname, row, col):
     
     ax2.set(title = dn.strftime('%H:%M (UT)'))
     
+    if col == 0:
+        ax2.text(0.01, 1.2, '(a)',
+             transform = ax2.transAxes)
+    
     return ax2
 
-def plot_ionogram(fname, row, col):
-    
-    ax2 = fig.add_subplot(gs[row, col])
-    
+def plot_ionogram(ax2, fname):
+        
     img = io.imread(fname, as_gray = False)
     
     ax2.imshow(dg.crop_image(img))
@@ -45,40 +45,17 @@ def plot_ionogram(fname, row, col):
     
     dn = dg.ionosonde_fname(fname) 
     ax2.set(title = dn.strftime('%H:%M (UT)'))
+    
+    if col == 0:
+        ax2.text(0.01, 1.2, '(b)',
+         transform = ax2.transAxes)
 
     return dn
 
 
-def plot_imager_digisonde(fn_skys):
-    
-    out = []
-    
-    for i, fn_sky in enumerate(fn_skys):
-                
-        imag = os.path.join(
-            PATH_IMAG,
-            fn_sky
-            )
-        
-        plot_imager(imag, 0, i)
-        
-        fn_iono = ev.get_closest_iono(fn_sky)
-        
-        iono = os.path.join(
-            PATH_IONO,
-            fn_iono
-            )
-        
-        dn = plot_ionogram(iono, 1, i)
-        
-        out.append(dn)
-        
-    return out
 
-def plot_roti(dn, out):
-        
-    ax1 = fig.add_subplot(gs[-1, :])
-    
+def plot_roti(ax1, dn, out):
+            
     path = f'database/epbs/vad_random/{dn.year}.txt'
     
     ds = b.sel_times(b.load(path), dn)
@@ -98,6 +75,9 @@ def plot_roti(dn, out):
             color = "gray"
         )
     
+    ax1.text(0.02, 0.85, '(c)', 
+             transform = ax1.transAxes)
+    
 
 fig = plt.figure(
     figsize = (10, 10),
@@ -105,8 +85,6 @@ fig = plt.figure(
     )
 
 ncols = 4
-
-gs = GridSpec(3, ncols, figure = fig)
 
 
 PATH_IMAG = 'imager/img/CA_2013_0114/'
@@ -121,11 +99,42 @@ fn_skys = [
 
 
 
-out = plot_imager_digisonde(fn_skys)
 
 dn = dt.datetime(2013, 1, 14, 20)
 
-plot_roti(dn, out)
 
+gs2 = GridSpec(3, ncols)
+
+gs2.update(hspace=0.2,  wspace = 0.)
+
+out = []
+
+for col, fn_sky in enumerate(fn_skys):
+    
+    imag = os.path.join(
+        PATH_IMAG,
+        fn_sky
+        )
+    
+    ax1 = plt.subplot(gs2[0, col])
+    
+    plot_imager(ax1, imag)
     
     
+    fn_iono = ev.get_closest_iono(fn_sky)
+    
+    iono = os.path.join(
+        PATH_IONO,
+        fn_iono
+        )
+    
+    ax2 = plt.subplot(gs2[1, col])
+    
+
+    out.append(plot_ionogram(ax2, iono))
+       
+
+ax3 = plt.subplot(gs2[-1, :])
+
+
+plot_roti(ax3, dn, out)
