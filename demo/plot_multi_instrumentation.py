9775cc85251d162020a3fa46bbd0079805de3d0c
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-10-27 17:04:12 -0300

    new varibles into macth instruments

--- demo/plot_multi_instrumentation.py
+++ demo/plot_multi_instrumentation.py
@@ -7,12 +7,12 @@ import os
 import imager as im
 import base as b 
 import datetime as dt 
-import numpy as np
+from plotting import plot_roti_curves
 
 def folder_date(dn):
     return dn.strftime('%Y%m%d')
 
-def plot_imager(ax2, fname):
+def plot_imager(ax2, fname, col):
     
     img = io.imread(fname, as_gray = True)
     
@@ -20,32 +20,34 @@ def plot_imager(ax2, fname):
         img, 
         cmap = 'gray'
         )
+       
+    ax2.set_axis_off()
     
-    ax2.tick_params(
-        labelbottom = False, 
-        labelleft = False
-        )
-    
-    dn  = im.imager_fname(fname).datetime
-    
-    ax2.set(title = dn.strftime('%H:%M'))
+    dn = im.imager_fname(fname).datetime
+    title = dn.strftime(f'({col + 1}) %H:%M')
+    ax2.set(title = title)
     
     if col == 0:
         ax2.text(
             0.03, 0.85, '(a)',
             color = 'white',
-            transform = ax2.transAxes)
+            transform = ax2.transAxes
+            )
     
     return ax2
 
-def plot_ionogram(ax2, fname):
+def plot_ionogram(ax2, fname, col):
         
     img = io.imread(fname)
     
-    ax2.imshow(dg.crop_image(
-        img, 
-        x = 170, 
-        w = 580)
+    ax2.imshow(
+        dg.crop_image(
+            img, 
+            y = 200,
+            h = 700, 
+            x = 150, 
+            w = 620
+            )
         )
     
     ax2.tick_params(
@@ -54,7 +56,10 @@ def plot_ionogram(ax2, fname):
         )
     
     dn = dg.ionosonde_fname(fname) 
-    ax2.set(title = dn.strftime('%H:%M'))
+    
+    title = dn.strftime(f'({col + 1}) %H:%M')
+    
+    ax2.set(title = title)
     
     if col == 1:
         ax2.text(0.6, -0.1, 'Frequency (MHz)',
@@ -74,32 +79,32 @@ def plot_ionogram(ax2, fname):
 
 
 
-def plot_roti(ax1, dn, out):
-            
-    path = f'database/epbs/vad_random/{dn.year}.txt'
+
+def plot_roti(ax1, dn, times):
     
-    ds = b.sel_times(b.load(path), dn)
-    
-    ax1.plot(ds)
-    
-    ax1.set(
-        yticks = np.arange(0, 5),
-        ylabel = 'ROTI (TECU/min)', 
-        ylim = [0, 5]
-        )
+    delta = dt.timedelta(minutes = 10)
+                
+    plot_roti_curves(ax1, dn)
     
     b.format_time_axes(ax1)
     
-    for n in out:
-        delta = dt.timedelta(minutes = 10)
+    for i, n in enumerate(times):
+        
+        ax1.text(
+            n, 4, i + 1, 
+            transform = ax1.transData
+            )
+        
         ax1.axvspan(
             n, n + delta,
-            alpha = 0.3, 
-            color = "gray"
+            alpha = 0.7, 
+            color = 'gray',
+            edgecolor = 'k', 
+            lw = 2
         )
     
     ax1.text(
-        0.03, 0.85, '(c)', 
+        0.02, 0.85, '(c)', 
         transform = ax1.transAxes
         )
     
@@ -110,18 +115,19 @@ def plot_multi_instrumentation(dn, fn_skys):
 
     fig = plt.figure(
         dpi = 400,
-        figsize = (8, 10),
+        figsize = (10, 10),
         layout = "constrained"
         )
     
     PATH_IMAG = f'imager/img/{im.folder_from_dn(dn)}/'
-    PATH_IONO = f'database/iono/{folder_date(dn)}/'
-    
+    PATH_IONO = f'digisonde/data/ionogram/{folder_date(dn)}S/'
 
-    
     gs2 = GridSpec(3, len(fn_skys))
     
-    gs2.update(hspace = 0.12,  wspace = 0)
+    gs2.update(
+        hspace = 0.2,  
+        wspace = 0
+        )
     
     out = []
     
@@ -134,10 +140,9 @@ def plot_multi_instrumentation(dn, fn_skys):
         
         ax1 = plt.subplot(gs2[0, col])
         
-        plot_imager(ax1, imag)
+        plot_imager(ax1, imag, col)
         
-        
-        fn_iono = ev.get_closest_iono(fn_sky)
+        fn_iono = ev.get_closest_iono(fn_sky, PATH_IONO)
         
         iono = os.path.join(
             PATH_IONO,
@@ -146,8 +151,7 @@ def plot_multi_instrumentation(dn, fn_skys):
         
         ax2 = plt.subplot(gs2[1, col])
         
-    
-        out.append(plot_ionogram(ax2, iono))
+        out.append(plot_ionogram(ax2, iono, col))
            
     
     ax3 = plt.subplot(gs2[-1, :])
@@ -157,14 +161,18 @@ def plot_multi_instrumentation(dn, fn_skys):
     
     return fig
 
-dn = dt.datetime(2013, 1, 14, 20)
+dn = dt.datetime(2013, 6, 10, 20)
 
 
-fn_skys = [
-    'O6_CA_20130114_224619.png', 
-    'O6_CA_20130114_231829.png',
-    'O6_CA_20130114_234329.png', 
-    'O6_CA_20130115_020958.png'
+fn_skys = [ 
+    'O6_CA_20130610_220827.png',
+    'O6_CA_20130610_225828.png', 
+    'O6_CA_20130611_001329.png', 
+    'O6_CA_20130611_014955.png'
     ]
 
 fig = plot_multi_instrumentation(dn, fn_skys)
+
+fig.savefig(b.LATEX('non_EPB_occurrence'), dpi = 400)
+
+
