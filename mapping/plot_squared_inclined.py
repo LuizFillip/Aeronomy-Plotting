3098f65a9e38c51dedfa61cbccc17e19ac5acbaf
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-11-01 19:27:34 -0300

    new map

--- mapping/plot_squared_inclined.py
+++ mapping/plot_squared_inclined.py
@@ -1,11 +1,12 @@
-import pyIGRF
 import numpy as np
 import cartopy.crs as ccrs
 import pandas as pd
 import GEO as gg
+import GNSS as gs 
+import os 
+import PlasmaBubbles as pb 
 
-
-def mapping():
+def mapping(year = 2013):
     
     lat_lims = dict(
         min = -25, 
@@ -23,75 +24,126 @@ def mapping():
         lat_lims = lat_lims, 
         lon_lims = lon_lims, 
         figsize = (9, 9), 
-        year = 2013, 
+        year = year, 
         degress = None
         )
     
     return ax
      
-radius = 10
 
-year = 2013
+def equator_coords(year = 2013):
 
-eq = gg.load_equator(year)
+    eq = gg.load_equator(year)
+    
+    return pd.DataFrame(
+        eq, columns = ['lon', 'lat']
+        )
 
-df = pd.DataFrame(
-    eq, columns = ['lon', 'lat']
-    )
+
+
+def corner_coords(
+        year = 2013, 
+        radius = 10,  
+        angle_degrees = -45
+        ):
+    
+    df = equator_coords(year)
+
+    x_coords = []
+    y_coords = []
+    
+    for slon in list(
+            range(-65, -30, radius)
+            ):
+        
+        coords = df.loc[
+            (df['lon'] > slon - radius) &
+            (df['lon'] < slon)
+            ].min()
+        
+        
+        clon = coords['lon']
+        clat = coords['lat']
+        
+        angle_radians = np.radians(angle_degrees)
+        
+        x_limits = []
+        y_limits = []
+        
+        for i in range(4):
+            angle = angle_radians + i * np.pi / 2  
+            x = clon + (radius - 3) * np.cos(angle)
+            y = clat + (radius + 3) * np.sin(angle) 
+            
+            x_limits.append(round(x, 4))
+            y_limits.append(round(y, 4))
+        
+        
+        x_limits.append(x_limits[0])
+        y_limits.append(y_limits[0])
+        
+        x_coords.append(x_limits)
+        y_coords.append(y_limits)
+    
+    return x_coords, y_coords
+            
+    
+
+
+
+def plot_corners(ax, clon, clat):
+    ax.scatter(clon, clat, c = 'k') 
+
+    for x_limits, y_limits in zip(x_coords, y_coords):
+        ax.plot(
+            x_limits,
+            y_limits,
+            color = 'black', 
+            linewidth = 2, 
+            transform = ccrs.PlateCarree() 
+            )
+
+def set_coords(
+        x_coords, 
+        y_coords
+        ):
+
+    coords = {}
+    
+    for i in range(4):
+        
+        lon_set = sorted(tuple(set(x_coords[i])))
+        lat_set = sorted(tuple(set(y_coords[i])))
+        
+        coords[i + 1] = (lon_set, lat_set)
+    
+    return coords
+
+
+
+
+
+
+year = 2019
+doy = 1
+path  = gs.paths(year, doy, root = os.getcwd())
+df = pb.load_filter(path.fn_roti)
+
+x_coords, y_coords = corner_coords(year)
 
 
 ax = mapping()
+coords = set_coords(x_coords, y_coords)
 
-for slon in list(
-        range(-65, -30, radius)
-        ):
-    
-    coords = df.loc[
-        (df['lon'] > slon - radius) &
-        (df['lon'] < slon)
-        ].min()
-    
-    
-    clon = coords['lon']
-    clat = coords['lat']
-    
-    lon_min = clon - radius
-    lat_min = clat - radius
-    
-    # d, _, _, _, _, _, _ = pyIGRF.igrf_value(
-    #     clat, 
-    #     clon, 
-    #     alt = 300, 
-    #     year = year
-    #     )
+(x0, x1), (y0, y1) = coords[1]
 
-    angle_degrees = -45
-    angle_radians = np.deg2rad(angle_degrees)
-    
-    x_limits = []
-    y_limits = []
-    
-    for i in range(4):
-        angle = angle_radians + i * np.pi / 2  
-        x = clon + (radius - 3) * np.cos(angle)
-        y = clat + (radius + 3) * np.sin(angle) 
-        x_limits.append(x)
-        y_limits.append(y)
-    
-    
-    x_limits.append(x_limits[0])
-    y_limits.append(y_limits[0])
-    
-    
-    ax.plot(
-        x_limits,
-        y_limits,
-        color = 'black', 
-        linewidth = 2, 
-        transform = ccrs.PlateCarree() 
-        )
-    
-    
-    ax.scatter(clon, clat)
-    
 
+ds  = df.loc[
+    (df['lat'] > y0) & (df['lat'] < y1) &
+    (df['lon'] > x0) & (df['lon'] < x1)
+    ]
+
+
+ax.scatter(ds.lon, ds.lat, c = ds.roti, s = 3)
+
+ds
