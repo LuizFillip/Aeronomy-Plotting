3b5c02dec1e4b656e746e83b36862512af6029b9
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-12-02 17:06:44 -0300

    new layout for roti and images

--- PlasmaBubbles/plot_roti_and_images.py
+++ PlasmaBubbles/plot_roti_and_images.py
@@ -14,7 +14,7 @@ def plot_roti(ax, dn, glon = -50):
 
     ds = b.sel_times(
             b.load(infile),
-            dn, 
+            dn
         )
     
     args = dict(
@@ -30,11 +30,11 @@ def plot_roti(ax, dn, glon = -50):
             twilight = 18
             )
     
-    delta = dt.timedelta(hours = 1.8)
     
-    ax.axvline(dusk, lw = 2)
+    ax.axvline(dusk, lw = 2.5, linestyle = '--')
     ax.text(
-        dusk - delta, 0.9, 
+        dusk, 
+        1.02, 
         'Terminator', 
         transform = ax.transData
         )
@@ -45,101 +45,129 @@ def plot_roti(ax, dn, glon = -50):
         ylabel = 'ROTI (TECU/min)'
         )
     
-    b.format_time_axes(ax)
+    b.format_time_axes(ax, hour_locator = 1)
+    
+    return None
     
     
 
-def plot_imager(ax2, fname, col):
+def plot_imager(ax, fname):
     
-    img = io.imread(fname, as_gray = True)
-    
-    ax2.imshow(
-        img, 
+    ax.imshow(
+        io.imread(fname, as_gray = True), 
         cmap = 'gray'
         )
        
-    ax2.set_axis_off()
-    
-    dn = im.imager_fname(fname).datetime
-    title = dn.strftime(f'({col + 1}) %H:%M')
-    ax2.set(title = title)
-    
-    return ax2
+    ax.set_axis_off()
 
-def plot_shade_bars(ax2, times, ytext = 0.85):
+    return ax
 
-    delta = dt.timedelta(minutes = 10)
+def plot_shade_bars(ax, times, ytext = 0.85):
+
+    delta = dt.timedelta(minutes = 12)
     
-    for index, dn in enumerate(times):
+    for index, time in enumerate(times):
         
-        ax2.text(
-            dn, 
+        ax.text(
+            time, 
             ytext, 
             index + 1, 
-            transform = ax2.transData
+            transform = ax.transData
             )
         
-        ax2.axvspan(
-            dn, 
-            dn + delta,
+        ax.axvspan(
+            time, 
+            time + delta,
             alpha = 0.7, 
             color = 'gray',
             edgecolor = 'k', 
             lw = 2
         )
+        
+    return None
 
-def plot_image_in_sequence(gs2, fn_skys):
+
+def plot_image_in_sequence(gs2, path_images):
 
     times = []
 
-    for col, file in enumerate(fn_skys):
+    for col, file in enumerate(path_images):
         
-        
-        ax1 = plt.subplot(gs2[0, col])
-        
-        plot_imager(ax1, file, col)
         fn = os.path.split(file)[-1]
-        times.append(im.fn2datetime(fn))
         
-    return ax1, times
+        dn = im.imager_fname(fn).datetime
+        times.append(dn)
+        title = dn.strftime(f'({col + 1}) %H:%M')
+        
+        
+        if col <= 1:
+            ax1 = plt.subplot(gs2[0, col + 2])
+            x, y = (.3, 1.05)
+            
+        elif col == 3:
+            ax1 = plt.subplot(gs2[1, 2])
+            x, y = (.3, -0.12)
 
-fn_skys = [ 
-    'database/CA_2017_0403P/O6_CA_20170404_022551.png',
-    'database/CA_2017_0403P/O6_CA_20170404_030245.png', 
-    'database/CA_2017_0403P/O6_CA_20170404_040009.png',
-    'database/CA_2017_0403P/O6_CA_20170404_061121.png'
-    ]
+        else:
+            ax1 = plt.subplot(gs2[1, 3])
+            x, y = (.3, -0.12)
+        
+       
+        ax = plot_imager(ax1, file)
+        
+        ax.text(
+            x, y, title, 
+            transform = ax.transAxes
+                       )
+    return times
 
 
-def plot_roti_and_images():
 
+def plot_roti_and_images(
+        dn,
+        path_images, 
+        fontsize = 30
+        ):
 
     fig = plt.figure(
         dpi = 300,
-        figsize = (12, 8),
-        layout = "constrained"
+        figsize = (16, 8),
+        layout = 'compressed'
         )
         
     gs2 = GridSpec(2, 4)
     
-    gs2.update(
-        hspace = 0,  
-        wspace = 0
-        )
+    gs2.update(hspace = 0, wspace = 0.0)
         
+    times = plot_image_in_sequence(gs2, path_images)
     
-    times = plot_image_in_sequence(gs2, fn_skys)
-    
-    ax2 = plt.subplot(gs2[-1, :])
-    
-    plot_shade_bars(ax2, times, ytext = 0.85)
-    
-    dn = dt.datetime(2017, 4, 3, 20)
+    ax2 = plt.subplot(gs2[:2, :2])
     
     plot_roti(ax2, dn)
     
+    plot_shade_bars(ax2, times, ytext = 0.85)
     
+    ax2.text(0, 1, '(a)', 
+             fontsize = fontsize,
+             transform = ax2.transAxes)
+    
+    ax2.text(1.0, 1, '(b)', 
+             fontsize = fontsize,
+             transform = ax2.transAxes)
+   
     return fig 
 
+path_images = [ 
+    'database/CA_2017_0403P/O6_CA_20170404_022551.png',
+    'database/CA_2017_0403P/O6_CA_20170404_030245.png',
+    'database/CA_2017_0403P/O6_CA_20170404_061121.png',
+    'database/CA_2017_0403P/O6_CA_20170404_040009.png'
+    
+    ]
 
+dn = dt.datetime(2017, 4, 3, 20)
+fig = plot_roti_and_images(dn, path_images)
 
+save_in = 'G:\\Meu Drive\\Doutorado\Travels\\O6_CA_20170404'
+
+fig.savefig(save_in, dpi = 300)
