485d0d2a948772930fcd7ac1b25ce38f92172e48
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-12-03 18:36:21 -0300

    removing

---
+++ demo/combine_images.py
@@ -0,0 +1,38 @@
+from skimage import io
+import os
+import matplotlib.pyplot as plt
+import GEO as gg 
+import cartopy.crs as ccrs
+
+
+def figure(year = 2014):
+    fig, ax = plt.subplots(
+        ncols = 3, 
+        dpi = 300,
+        sharey = True,
+        sharex = True,
+        figsize = (14, 10),
+        subplot_kw={
+            'projection': ccrs.PlateCarree()}
+        )
+    
+    plt.subplots_adjust(wspace= 0.1)
+    
+    for i in range(3):
+        gg.map_attrs(ax[i], year)
+    
+
+    return fig, ax
+        
+def run():
+    for fn in os.listdir('temp0/'):
+        print(fn)
+        plt.ioff()
+        # fig = combine(fn)
+        # fig.savefig(f'temp3/{fn}', dpi = 300)
+        plt.clf()   
+        plt.close()
+    
+fig, ax = figure(year = 2014)
+
+plt.show()
