53ae5abca81398e182aa32875a1343af166f8e53
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2024-01-11 13:11:53 -0300

    adding square regions

--- mapping/plot_single_tec_map.py
+++ mapping/plot_single_tec_map.py
@@ -1,27 +1,15 @@
-import os
 import pandas as pd
 import numpy as np
 import cartopy.crs as ccrs
 import datetime as dt
-import matplotlib as mpl
 import matplotlib.pyplot as plt
 import GEO as g
 import base as b 
+import plotting as pl
+
 
 b.config_labels(fontsize = 25)
 
-
-def tec_fname(filename: str) -> dt.datetime:
-    """Convert TEC filename (EMBRACE format) to dt"""
-    args = filename.split('_')
-    date = args[1][:4] + '-' + args[1][4:6]+ '-' +args[1][-2:] 
-    time = args[-1].replace('.txt', '')
-    time = time[:2] + ':' + time[2:]
-    
-    return dt.datetime.strptime(
-        date + ' ' + time, 
-        "%Y-%m-%d %H:%M"
-        )
 
 def load_tec(infile):
 
@@ -38,23 +26,29 @@ def load_tec(infile):
          
     return lon, lat, df.values
 
-
-
 def plot_contourf(ax, lon, lat, values, step = 5):
     
-    v = np.arange(0, 80 + step, step*0.5)
+    v = np.arange(0, 70 + step, step*0.5)
     
-    img = ax.contourf(lon, lat, values, levels = v,
-                      cmap = 'rainbow')
+    img = ax.contourf(
+        lon, lat, values, 
+        levels = v,
+        cmap = 'rainbow'
+        )
     
- 
-    b.colorbar(img, ax, v, 
-            label = r'TEC ($10^{16} / m^2$)')
+    b.colorbar(
+        img, ax, v, 
+        label = r'TEC ($10^{16} / m^2$)'
+        )
     
     return img
 
+def get_path(dn):
+    infile = "D:\\database\\"
+    fmt = 'TEC_%Y\\TEC_%Y_%m\\TECMAP_%Y%m%d_%H%M.txt'
+    return infile +  dn.strftime(fmt)
 
-def plot_tec_map(infile, fontsize = 25):
+def plot_tec_map(dn):
 
     fig, ax = plt.subplots(
          figsize = (10, 10), 
@@ -63,26 +57,23 @@ def plot_tec_map(infile, fontsize = 25):
          {'projection': ccrs.PlateCarree()}
          )
     
-    lon, lat, vls =  load_tec(infile)
+    lon, lat, vls =  load_tec(get_path(dn))
     
     plot_contourf(ax, lon, lat, vls)
     g.mag_equator(ax)
     g.map_features(ax)
     g.map_boundaries(ax)
+    pl.plot_corners(ax, dn.year)
     
-    fname = os.path.split(infile)[-1]
+    term_lon, term_lat = g.terminator2(dn, 18)
     
-    
-    ax.set(title = tec_fname(
-        fname).strftime('%Y/%m/%d %H:%M'))
+    ax.scatter(term_lon, term_lat, s = 10)
+    ax.set(title = dn.strftime('%Y/%m/%d %H:%M (UT)'))
     
     return fig
 
-def get_path(dn):
-    infile = "D:\\database\\"
-    fmt = 'TEC_%Y\\TEC_%Y_%m\\TECMAP_%Y%m%d_%H%M.txt'
-    return infile +  dn.strftime(fmt)
 
-dn = dt.datetime(2013, 12, 25, 23)
 
-fig = plot_tec_map(get_path(dn), fontsize = 40)
+dn = dt.datetime(2013, 12, 25, 1)
+
+fig = plot_tec_map(dn)
