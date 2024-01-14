2664b9f3011c7195233c9ed7b63762139070261b
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2024-01-14 11:32:06 -0300

    replacing and deleting map

--- mapping/plot_regions_over_map.py
+++ mapping/plot_regions_over_map.py
@@ -3,7 +3,14 @@ import GEO as gg
 import matplotlib.pyplot as plt
 import base as b 
 
-b.config_labels()
+b.config_labels(fontsize = 30)
+
+args = dict( 
+    s = 40, 
+    marker = '^',
+    color = 'k', 
+    transform = ccrs.PlateCarree()
+    )
 
 
 def plot_regions_over_map(year = 2013):
@@ -16,14 +23,23 @@ def plot_regions_over_map(year = 2013):
     fig, ax = plt.subplots(
         dpi = 300,
         sharex = True, 
-        figsize = (10,10),
+        figsize = (10, 10),
         subplot_kw = {'projection': ccrs.PlateCarree()}
     )
     
-    gg.map_attrs(ax, year)
+    gg.map_attrs(ax, year, degress = 5)
     gg.plot_rectangles_regions(ax)
     
-
+    
+    sites = gg.stations_near_of_equator(
+        year,
+        distance = 5
+        )
+    
+    for (lon, lat) in sites.values():
+        
+        ax.scatter(lon, lat, **args)
+    
     
     return fig
     
