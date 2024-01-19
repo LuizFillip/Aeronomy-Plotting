6926a305583e2297df1dac75abb61cd5b5480928
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2024-01-18 23:30:11 -0300

    new plots

--- FluxTube/plot_apex_vs_latitude.py
+++ FluxTube/plot_apex_vs_latitude.py
@@ -22,64 +22,9 @@ def infos(ax):
         color = 'k'
         )
     
-
-def plot_apex_vs_latitude(
-       
-        lim = 15,
-        color = 'k'
-        ):
-    fig, ax = plt.subplots(
-        figsize = (12, 6),
-        dpi = 300
-        )
-
-    max_height = 500
-    step = 50
-    base = 75
-    heights = np.arange(50, max_height + step, step )
-     
-   
-    for h in heights:
-        
-        apx = Apex(h) 
-        lats =  apx.latitude_range(
-            points = 30, 
-            base = 150
-            )
-        apex = apx.apex_range(
-            points = 30, 
-            base = base
-            )
-        
-        if h == 300:
-            args = dict(
-                marker = 'o', 
-                markerfacecolor = 'w', 
-                lw = 2)
-        else:
-            args = dict(color = 'k', lw = 2)
-            
-        ax.plot(np.degrees(lats), apex, **args)    
     
-    lat_mag = 3
-    zeq = Apex(400).apex_height(lat_mag)
-        
-    ax.arrow(
-        x = lat_mag, y= 0, 
-        dx = 0, dy=zeq, width= 0.1, 
-        color='red', head_length = 5,
-        label = 'Altura local') 
+def plot_hlines(ax):
     
-    ax.set(
-        xlim = [-lim, lim],
-        ylim = [100, max_height],
-        ylabel = "Altura de Apex (km)", 
-        xlabel = "Latitude magnética (°)"
-        )
-    
-    ax.axvline(0, linestyle = "--")
-
-        
     ax.axhline(
         150, 
         color = "red", 
@@ -94,6 +39,69 @@ def plot_apex_vs_latitude(
         lw= 2,
         linestyle = "--"
         )
+
+def plot_arrow(ax, lat_mag = 3):
+    
+    zeq = Apex(400).apex_height(lat_mag)
+        
+    ax.arrow(
+        x = lat_mag, y= 0, 
+        dx = 0, dy=zeq, width= 0.1, 
+        color='red', head_length = 5,
+        label = 'Altura local') 
+
+def plot_apex_vs_latitude(
+       
+        lim = 15,
+        color = 'k'
+        ):
+    fig, ax = plt.subplots(
+        figsize = (10, 10),
+        dpi = 300
+        )
+
+    max_height = 550
+    step = 100
+    base = 75
+    heights = np.arange(base, max_height, step )
+     
+   
+    for h in heights:
+        
+        apx = Apex(h) 
+        lats =  apx.latitude_range(
+            points = 30, 
+            base = 150
+            )
+        apex = apx.apex_range(
+            points = 30, 
+            base = base
+            )
+        
+        # if h == 300:
+        #     args = dict(
+        #         marker = 'o', 
+        #         markerfacecolor = 'w', 
+        #         lw = 2)
+        # else:
+        #     args = dict(color = 'k', lw = 2)
+        
+        args = dict(color = 'k', lw = 2)
+        ax.plot(np.degrees(lats), apex, **args)    
+    
+    
+    
+    ax.set(
+        xlim = [-lim, lim],
+        ylim = [75, max_height],
+        ylabel = "Altura de Apex (km)", 
+        xlabel = "Latitude magnética (°)"
+        )
+    
+    ax.axvline(0, linestyle = "--", lw = 1)
+
+    
+    plot_hlines(ax)
     
     # ax.text(
     #     0.03, 0.91, '(b)', 
@@ -104,8 +112,8 @@ def plot_apex_vs_latitude(
     return fig 
 
 
-# fig = plot_apex_vs_latitude()
+fig = plot_apex_vs_latitude()
 
-# FigureName = 'magnetic_lines'
+FigureName = 'magnetic_lines'
 
-# fig.savefig(b.LATEX(FigureName, folder = 'modeling'), dpi = 400)
+fig.savefig(b.LATEX(FigureName, folder = 'modeling'), dpi = 400)
--- FluxTube/utils.py
+++ FluxTube/utils.py
@@ -1,10 +1,16 @@
 import pandas as pd 
 
 
-def load_fluxtube(dn = '2013-12-24 22:00:00'):
+
+dn = '2013-12-24 22:00:00'
+
+
+def load_sep(dn = dn):
     
     infile = "20131224sep.txt"
     
     df = pd.read_csv(infile, index_col = 0)
     
     return df.loc[df['dn'] == dn]
+
+
