8f607488d2682d02854f233cfccbb29cfc9f732b
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2024-02-23 10:46:48 -0300

    plot more infos

--- ionosonde/plot_vz_and_frequencies.py
+++ ionosonde/plot_vz_and_frequencies.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 import digisonde as dg
-import base as s
+import base as b 
 import GEO as gg 
 import datetime as dt
 
 
-s.config_labels(fontsize = 25)
+b.config_labels(fontsize = 25)
 
 def plot_terminators(ax, dn):
          
@@ -24,7 +24,6 @@ def plot_terminators(ax, dn):
              dusk + delta,
              alpha = 0.2, 
              color = 'gray',
-             edgecolor = 'k', 
              lw = 2
          )
              
@@ -39,17 +38,27 @@ def plot_terminators(ax, dn):
 
 def plot_infos(ax, vz, dn):
     
-    idx, vmax = dg.time_between_terminator(vz, dn)
+    data = dg.time_between_terminator(vz, dn)
+    idx = data['time']
+    vmax = data['vp']
+    ax.axvline(idx, label = 'Vp')
+    
     idx = idx.strftime('%Hh%M')
+    
     vmax = round(vmax, 2)
-    ax.text(0.5, 0.8,
+    
+    ax.text(0.05, 0.1,
             f'Vp = {vmax} m/s ({idx} UT)',
             transform = ax.transAxes)
+    
+    ax.axhline(0, linestyle = '--')
+    
+    ax.legend(loc = 'upper right')
     
 def plot_vz_and_frequencies(df, vz, dn):
     
     df = df.iloc[1:]
-    vz = vz.iloc[1:]
+    vz = vz.dropna().iloc[1:]
     
     fig, ax = plt.subplots(
         figsize = (12, 8), 
@@ -60,7 +69,10 @@ def plot_vz_and_frequencies(df, vz, dn):
     
     plt.subplots_adjust(hspace = 0.05)
     
-    
+    ax[1].plot(vz['vz'], lw = 2, 
+               linestyle = '--',
+               color = 'k', 
+               label = 'Média')
     
     for num, col in enumerate(df.columns):
         
@@ -68,8 +80,9 @@ def plot_vz_and_frequencies(df, vz, dn):
             
             ax[0].plot(df[col], label = f'{col}')
             
-            ax[1].plot(vz[col], label = f'{col}')
-
+            ax[1].plot(vz[col])
+            
+    
 
     ax[0].set(ylabel = "Altitude (km)", 
               ylim = [100, 700])
@@ -82,7 +95,7 @@ def plot_vz_and_frequencies(df, vz, dn):
               xlim = [df.index[0], df.index[-1]]
               )
      
-    s.format_time_axes(ax[1], translate = True)
+    b.format_time_axes(ax[1], translate = True)
     
     ax[0].legend(
         bbox_to_anchor = (.5, 1.45), 
@@ -93,9 +106,7 @@ def plot_vz_and_frequencies(df, vz, dn):
     
     plot_terminators(ax, dn)
     
-    ax[1].axhline(0, linestyle = '--')
-    
-    s.plot_letters(ax, y = 0.85, x = 0.03)
+    b.plot_letters(ax, y = 0.85, x = 0.03)
     
     plot_infos(ax[1], vz, dn)
 
@@ -103,22 +114,26 @@ def plot_vz_and_frequencies(df, vz, dn):
 
 
 
-   
 
         
 def main():
         
-    year = 2015
-    infile =  f'database/jic/freq/{year}'
-    df = dg.freq_fixed(infile)
-    
-    dn = dt.datetime(year, 1, 10, 20)
-    
-    ds = s.sel_times(df, dn, hours = 7) 
-    
+    infile = 'digisonde/data/jic_freqs.txt'
+
+    df = b.load(infile)
+    df = df.drop(columns = ['8', '9'])
+    dn = dt.datetime(2015, 1, 6, 20)
+
+    ds = b.sel_times(df, dn, hours = 7).interpolate()
+
     vz = dg.vertical_drift(ds)
-    
     
     fig = plot_vz_and_frequencies(ds, vz, dn)
     
 # main()
+
+# infile = 'digisonde/data/jic_freqs.txt'
+
+# df = b.load(infile)
+
+# df
