240d53385083eb8dc1bea416572f356053ab0af6
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2024-03-17 18:11:16 -0300

    updates layouts

--- climatologies/plot_roti_solar_magnetic_activity.py
+++ climatologies/plot_roti_solar_magnetic_activity.py
@@ -1,47 +1,47 @@
 import matplotlib.pyplot as plt 
 import base as b 
-import datetime as dt 
+import plotting as pl 
+import core as c 
 
-def plot_year(df, ax, col, year = 2019):
-    ds = df.loc[df.index.year == year]
-    ds.index = ds.index.day_of_year
-    ds[col].plot(ax = ax, label = year)
-    ax.legend()
-    # b.format_month_axes(ax)
+
+
+def plot_roti_solar_magnetic_activity():
     
-    
-def plot_compare_solar_activity(
-        col = 'K', 
-        site = 'saa'
-        ):
-    
+    df = b.load('all_maximus')
+
 
     fig, ax = plt.subplots(
-        sharey = True,
         sharex = True,
         dpi = 300, 
-        nrows = 2,
-        figsize = (14, 8), 
+        nrows = 3,
+        figsize = (16, 12), 
         )
-    
-   
-   
-    plot_year(df, ax[0], col, year = 2013)
-    plot_year(df, ax[1], col, year = 2019)
-    
-    
-    
-    
-# plot_compare_solar_activity()
-infile = 'hourly_roti.txt'
-df = b.load(infile)
 
-# ds = df.resample('1H').mean()
+    plt.subplots_adjust(hspace = 0.1)
 
-# dn = dt.datetime(2013, 5, 15, 20)
 
-# ds = b.sel_times(df, dn, hours = 13)
+    ds = df.resample('1D').mean()
 
-# ds.plot(ylim = [0, 3])
+    idx = c.geo_index(
+            cols = ['f107a', 'f107', 'kp', 'dst'],
+            syear = 2013, 
+            eyear = 2023
+            )
 
-df['-50'].plot()
+    pl.plot_F107(ax[1], idx, solar_level = None)
+    pl.plot_Kp(ax[0], idx, kp_level = None)
+    ax[2].plot(ds['-50'])
+
+    ax[2].set(xlim = [ds.index[0], ds.index[-1]], 
+              xlabel = 'Anos', ylabel = 'ROTI (TECU/min)')
+
+    b.plot_letters(ax, y = 0.8, x = 0.02)
+    
+    return fig
+    
+    
+fig = plot_roti_solar_magnetic_activity()
+
+
+# FigureName = 'annual_variation_roti_and_indices'
+# fig.savefig(b.LATEX(FigureName, folder = 'climatology'))
