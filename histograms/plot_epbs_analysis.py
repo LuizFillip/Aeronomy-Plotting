1ce4f7518735dc9beffb2639385f9f34903ad498
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2024-03-18 19:09:50 -0300

    updates

--- histograms/plot_epbs_analysis.py
+++ histograms/plot_epbs_analysis.py
@@ -2,61 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import base as b
 
-
-b.config_labels()
-args = dict(
-    facecolor='lightgrey', 
-    alpha=1, 
-    edgecolor = 'black',  
-    color = 'gray'
-    )
-
-
-def plot_epbs_stats(ds, fontsize = 30):
-    
-    
-    fig, ax = plt.subplots(
-        ncols = 4,
-        nrows = 3,
-        figsize = (14, 12),
-        dpi = 300,
-        sharey = 'row'
-        )
-    
-    
-    plt.subplots_adjust(wspace = 0.05, hspace = 0.5)
-    
-    
-    for col, lon in enumerate(ds.lon.unique()):
-        
-        df = ds.loc[ds['lon'] == lon]
-        
-        df['start'].plot(
-            kind = 'hist', 
-            ax = ax[0, col], 
-            bins = np.arange(20, 30, 0.5), 
-            **args
-            )
-        
-        df['shift'].plot(
-            kind = 'hist', 
-            ax = ax[1, col], 
-            bins =  np.arange(-1, 8, 0.5), 
-            **args 
-            )
-        
-        df['duration'].plot(
-            kind = 'hist', 
-            ax = ax[2, col], 
-            bins = np.arange(-1, 12, 0.5), 
-            **args
-            )
-        
-        ax[0, col].set(title = f'{lon}°')
-        
-        if col < 3:
-            ax[col, 0].set(ylabel = '')
-         
+def figure_labels(fig, fontsize = 30):
+     
     fig.text(
         0.3, .63, 
         'Primeira ocorrência (Hora universal)', 
@@ -82,12 +29,106 @@ def plot_epbs_stats(ds, fontsize = 30):
         'Período de duração (horas)', 
         fontsize = fontsize
         )
+
+b.config_labels()
+
+
+args = dict(
+    facecolor='lightgrey', 
+    alpha=1, 
+    edgecolor = 'black',  
+    color = 'gray'
+    )
+
+
+
+
+def plot_info(ax, df, parameter):
     
+    vls = df[parameter]
+    mu = round(vls.mean(), 1)
+    sigma = round(vls.std(), 1)
+    
+    info =  f'$\mu = $ {mu} horas\n$\sigma = $ {sigma} horas'
+    
+    ax.text(0.5, 0.7, info, transform = ax.transAxes)
+    
+def bins(df, parameter = 'start', n = 10):
+    vmin = round(df[parameter].min())
+    vmax = round(df[parameter].max())
+
+    return np.linspace(vmin, vmax, n)
+
+
+
+def plot_epbs_stats(
+        ds, 
+        parameter = 'start', 
+        fontsize = 30
+        ):
+    
+    if parameter == 'start':
+        n = 24
+    else:
+        n = 50
+        
+    fig, ax = plt.subplots(
+        ncols = 2,
+        nrows = 2,
+        figsize = (12, 10),
+        dpi = 300,
+        sharex = True, 
+        sharey = True
+        )
+    
+    b.plot_letters(ax, y = 0.83, x = 0.04)
+    
+    plt.subplots_adjust(wspace = 0.05, hspace = 0.2)
+    
+    lons = ds['lon'].unique()[::-1]
+    
+    for i, ax in enumerate(ax.flat):
+        
+        df = ds.loc[ds['lon'] == lons[i]]
+        
+        plot_info(ax, df, parameter)
+
+        df[parameter].plot(
+            kind = 'hist', 
+            ax = ax, 
+            bins = bins(df, parameter, n), 
+            **args
+            )
+        
+        ax.set(title = f'{lons[i]}°')
+        
+        ax.set( 
+            ylabel = '',
+            ylim = [0, 1000],
+            xticks = bins(df, parameter, 5)
+            )
+    
+    fig.text( 
+        0.4, 0.05,
+        'Hora universal'
+             , fontsize = fontsize)
+    
+    fig.text(
+        0.03, .32, 'Frequência de ocorrência', 
+        rotation = 'vertical', fontsize = fontsize
+        )
+      
     return fig
 
 
-df = b.load('events_2013_2023')
-df = df.loc[~((df['lon'] == -80) & 
-              (df['start'] < 21))]
+df = b.load('events_2013_2023_2')
 
-fig = plot_epbs_stats(df, fontsize = 30)
+fig = plot_epbs_stats(df, fontsize = 30, parameter = 'duration')
+
+# FigureName = 'start_hour_occurrence'
+
+# fig.savefig(b.LATEX(FigureName, folder = 'histograms'), 
+#             dpi = 400)
+
+# bins = np.arange(-1, 12, 0.5)
+
