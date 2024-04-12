33ae73195f98befc5c2663c3f1eeed6b50e12740
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2024-04-11 22:13:02 -0300

    updates

--- climatologies/plot_GRT_and_EPBs.py
+++ climatologies/plot_GRT_and_EPBs.py
@@ -1,89 +1,41 @@
 import matplotlib.pyplot as plt
 import base as b 
-import events as ev 
+import core as c
 
-def plot_gamma(ax, df):
-    
-    # df = df * 1e3
-    
-    ax.scatter(
-        df.index, df, 
-        s = 30,
-        alpha = 0.4, 
-        color = 'gray'
-        )
-    
-    avg = b.smooth2(df, 20)
-    ax.plot(
-        df.index, 
-        avg, 
-        lw = 2
-        )
-    
-    ax.set(
-        ylim = [-0.2, 3], 
-        ylabel = b.y_label('gamma'),
-        xlim = [df.index[0], df.index[-1]]
-        )
-    
-    ax.axhline(avg.max(), linestyle = '--')
-    ax.axhline(avg.min(), linestyle = '--')
-    
-    return df
+
 
 
 b.config_labels()
 
-lbs = b.Labels().infos
+def plot_percent_contribuitions():
 
-
-fig, ax = plt.subplots(
-    sharex = True,
-    dpi = 400, 
-    nrows = 3, 
-    figsize = (14, 8), 
-    )
-
-plt.subplots_adjust(hspace = 0.1)
-
-df = ev.concat_results('saa', class_epb = 'sunset')
-
-
-plot_gamma(ax[1], df['gamma'])
-
-df = ev.epbs(geo = True)
-ax[0].plot(df['f107'])
-ax[0].plot(df['f107a'], lw = 2)
-ax[0].set(ylabel = 'F10.7 (sfu)')
-ax[0].axhline(86, lw = 2, color = 'r')
-ds = ev.month_to_month_occurrence(
-        df, 
-        col = 'epb'
+    fig, ax = plt.subplots(
+        sharex = True,
+        dpi = 300, 
+        figsize = (14, 8), 
         )
-
-args = dict(
-    facecolor = 'lightgrey', 
-    edgecolor = 'black', 
-    # width = 0.5,
-    width = 20,
-    color = 'gray', 
-    linewidth = 1
-    )
-
-
-ax[2].bar(ds.index, ds['epb'].ravel(), **args)
-ax[2].set(ylabel = 'Nights with EPBs',
-           ylim = [0, 40],
-           xlabel = 'Years'
-          )
-
-for i, ax in enumerate(ax.flat):
     
-    l = b.chars()[i]
-    ax.text(
-        0.02, 0.85, f'({l})', 
-        transform = ax.transAxes
-        )
+    plt.subplots_adjust(hspace = 0.1)
 
-plt.show()
+    df = c.gamma('saa', el_upper = False)
+
+    df = df.resample('1M').mean()
+    
+    cols = ['drift', 'gravity', 'winds', 'gamma']
+    
+    for col in cols:
+        
+        ax.plot((df[col] / df['gamma']) * 100, label = col)
+        
+    ax.legend(
+        ncol = 4, 
+        loc = 'upper center', 
+        bbox_to_anchor = (0.5, 1.2)
+        )
+    
+    ax.set(ylabel = 'Percentual de contribuição (\%)')
+
+# plot_percent_contribuitions()
+
+# df = c.gamma('saa', el_upper = False )
 
