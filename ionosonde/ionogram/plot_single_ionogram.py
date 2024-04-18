75451c2ad2489fbbae06e042a247e42db55f9b06
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2024-04-18 14:21:32 -0300

    new kword in ionogram

--- ionosonde/ionogram/plot_single_ionogram.py
+++ ionosonde/ionogram/plot_single_ionogram.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 from skimage import io
 import numpy as np
-
+import digisonde as dg 
 
 def redefine_ticks(ax, img):
     
@@ -30,15 +30,22 @@ def crop_region_E(img):
     x, w = 186, 560
     return img[y: y + h, x: x + w]
 
-def plot_single_ionogram(fname, ax = None, label = False, 
-                         aspect = 'auto'):
+def plot_single_ionogram(
+        fname, 
+        ax = None, 
+        label = False, 
+        ylabel_position = "left",
+        aspect = 'auto', 
+        title = False
+        ):
     
-    # xticklabels = [ 0,  '',  4,  '',  
-                   # 8,  '', 12, '', 16]
+  
     if ax is None:
         fig, ax = plt.subplots(
             figsize = (10, 8),
             dpi = 300)
+        
+        
     
     img = io.imread(fname)
     img = crop_region_E(img)
@@ -50,38 +57,62 @@ def plot_single_ionogram(fname, ax = None, label = False,
         redefine_ticks(ax, img)
         
         ax.set(
-            # xticklabels = xticklabels,
             ylabel = 'Altitude (km)', 
             xlabel = 'Frequency (MHz)'
             )
+        
+        if ylabel_position == 'left':
+            labelleft = True
+            labelright = False
+        else:
+            labelright = True
+            labelleft = False
+        
         
         ax.tick_params(
             top = True,
             right = True,
             left = False,
             bottom = False,
-            labelright = True,
-            labelleft = False,
+            labelright = labelright,
+            labelleft = labelleft,
             # labeltop = True, 
             # labelbottom = False, 
             # 
             )
-        ax.yaxis.set_label_position("right")
+        ax.yaxis.set_label_position(ylabel_position)
         
     else:
         ax.axis('off')
     
-  
+    if title:
+        time = dg.ionosonde_fname(fname)
+
+        ax.set(title = time.strftime('%Y-%m-%d %Hh%M'))
+
+    return fig
+
+infile = 'database/ionogram/20130114S/'
+
+def run():
+    import os 
+    
+    for file in os.listdir(infile):
+        if 'PNG' in file:
+            dn = dg.ionosonde_fname(file)
+            
+            plt.ioff()
+            
+            fig = plot_single_ionogram(
+                os.path.join(infile, file), 
+                label = True, 
+                title = True
+                )
+            
+            FigureName = dn.strftime('%Y%m%d%H%M')
+            
+            fig.savefig('temp/' + FigureName)
+            plt.close()
     
     
-    if ax is None:
-        
-        return fig
-    else:
-        return ax
-
-# fname = 'database/ionogram/20130610S/SAA0K_20130610(161)211000.PNG'
-# fig = plot_single_ionogram(fname, label = True)
-
-
-# plt.show()
+    # plt.show()
