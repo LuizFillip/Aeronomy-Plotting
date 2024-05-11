3db1862c16aa4a92a5a4d12eb87ad999ca725d28
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2024-05-11 12:42:20 +0900

    prediction for gamma and EPBs

--- forecast/plot_gamma_probability.py
+++ forecast/plot_gamma_probability.py
@@ -4,55 +4,66 @@ import base as b
 
 b.config_labels()
 
-
-
-def plot_gamma_predict_epbs(obs, pred):
+def plot_gamma_predict_epbs(df):
     
     fig, ax = plt.subplots(
         dpi = 300, 
         nrows = 3,
         sharex=True,
-        figsize = (12, 10)
+        figsize = (14, 12)
         )
     
     plt.subplots_adjust(hspace = 0.1)
     
-    ax[0].plot(obs['gamma'])
+    ax[0].plot(df['gamma'])
     
-    ax[1].plot(pred *100, color = 'k')
+    ax[1].plot(df['predict'] *100, color = 'k')
     
-    ax[2].plot(obs['epb'], color = 'k')
+    ax[2].plot(df['epb'], color = 'k', label = 'observadas')
+    ax[2].plot(df['epb_est'], color = 'b', label = 'Estimadas')
+    ax[2].legend(ncol = 2, loc = 'upper center')
     
     ax[1].set(
-        ylabel = 'Probability (\%)', 
+        ylabel = 'Probabilidade (\%)', 
         ylim = [-20, 120], 
         yticks = list(range(0, 125, 25))
         )
   
     ax[0].set(
-        ylabel = '$\gamma_{RT}$', 
+        ylabel = '$\gamma_{RT} ~(10^{-3}~s^{-1})$', 
         yticks = list(range(0, 4, 1))
         )
     
     ax[2].set(
-        ylabel = 'EPB observed', 
-        ylim = [-0.2, 1.2], 
+        ylabel = 'Ocorrência de EPBs', 
+        ylim = [-0.2, 1.4], 
+        xlim = [df.index[0], df.index[-1]],
         yticks = [0, 1]
         )
-    b.format_month_axes(ax[2])
+    
+    b.format_month_axes(ax[2], translate = False)
     
     for line in [0, 1]:
         
         ax[1].axhline(line * 100, lw = 0.5, linestyle = '--') 
         ax[2].axhline(line, lw = 0.5, linestyle = '--')
+    
+    b.plot_letters(ax, y = 0.88, x = 0.02)
+    
+    return fig 
 
-parameter = 'gamma'
 
-df = c.load_results('saa', eyear = 2023)
-obs = df.loc[df.index.year == 2023, ['gamma', 'epb']]
-pred = c.forecast_epbs(
-    year_threshold = 2023, 
-    parameter= 'gamma')
-pred = pred.data['predict']
+def main():
+    
+    df = c.load_results('saa', eyear = 2023)
+    ds = c.predict_seasons(df)
+    
+    fig = plot_gamma_predict_epbs(ds)
+    
+    FigureName = 'Estimation_2023'
+    
+    fig.savefig(b.LATEX(FigureName, folder = 'climatology'))
+    
+    
+# main()
 
-plot_gamma_predict_epbs(obs, pred)
