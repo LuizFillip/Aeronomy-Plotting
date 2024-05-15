48538e07c6ccf54e94593e716dba48ac1a394382
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2024-05-15 18:00:36 +0900

    updates

--- averages/plot_monthly_FPI_directions.py
+++ averages/plot_monthly_FPI_directions.py
@@ -2,10 +2,13 @@ import matplotlib.pyplot as plt
 import FabryPerot as fp
 import pandas as pd 
 import base as b 
+import datetime as dt 
+import numpy as np 
+
 
 b.config_labels()
 
-def plot_montly_averages(df):
+def plot_montly_averages(df, parameter = 'tn'):
 
     dirs = fp.DIRECTIONS
     
@@ -20,25 +23,50 @@ def plot_montly_averages(df):
         )
     
     plt.subplots_adjust(wspace = 0.05)
+    
+    dn = dt.datetime(2022, 7, 24, 21)
+    ds1 = b.sel_times(df, dn)
+    
+     
+    if parameter == 'tn':
+        ylabel = 'Temperature (K)'
+        ylim = [700, 1500]
+        yticks = np.arange(700, 1600, 200)
+    else:
+        ylabel = 'Velocity (m/s)'
+        ylim = [-100, 300]
+        yticks = np.arange(-100, 300, 50)
+        
+        
     for num, ax in enumerate(ax.flat):
         
+        col = dirs[num]
+        # print(col)
         ds = pd.pivot(
             df, 
             columns = 'day', 
             index = 'time', 
-            values = dirs[num]
+            values = col 
             )
-        
+        ax.plot(ds1['time'], ds1[col], color = 'r', lw = 2,
+                label = dn.strftime('%Y-%m-%d'))
         ax.plot(ds, alpha = 0.5, color = 'gray')
         ax.plot(ds.mean(axis = 1), color = 'k', lw = 3)
         
-        ax.set(title = dirs[num].capitalize())
-        ax.axhline(0, lw = 1.5, linestyle = '--')
+        ax.set(title = dirs[num].capitalize(), 
+               ylim = ylim,
+               yticks = yticks
+               )
+        ax.legend(loc = 'upper right')
+        # ax.axhline(0, lw = 1.5, linestyle = '--')
     
     fontsize = 30
+   
+        
+        
     fig.text(
         0.05, 0.35, 
-        'Velocity (m/s)', 
+        ylabel, 
         fontsize = fontsize, 
         rotation = 'vertical'
         )
@@ -49,11 +77,11 @@ def plot_montly_averages(df):
         fontsize = fontsize, 
         )
     
-    fig.suptitle(df.index[0].strftime('%B'))
-    
+    fig.suptitle(df.index[0].strftime('%Y-%B'))
+
     return fig
 
-
-# infile = 'FabryPerot/data/FPI/9/'
-# df = fp.join_days(infile)
-# fig = plot_montly_averages(df)
+infile = 'database/FabryPerot/cj/'
+df = fp.join_days(infile, parameter = 'tn')
+df  = df.loc[~(df['west'] > 1500)]
+fig = plot_montly_averages(df, parameter = 'tn')
