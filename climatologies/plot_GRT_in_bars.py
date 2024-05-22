b54592d78667f92334634060c2e232ae7c03937a
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2024-05-22 17:23:11 +0900

    new sazonality check values

--- climatologies/plot_GRT_in_bars.py
+++ climatologies/plot_GRT_in_bars.py
@@ -9,32 +9,34 @@ PATH_GAMMA = 'database/gamma/p1_saa.txt'
 df = b.load(PATH_GAMMA)
 
 df = df.loc[(df.index.time == dt.time(22, 0)) & 
-            (df.index.year < 2023)]
+            (df.index.year < 2015)]
 
 df['gr'] = df['ge'] / df['nui']
 df['gamma'] = df['gamma'] *1e3
 df['K'] = df['K'] * 1e5 
 
-ds = df.resample('1Y').mean()
+ds = df.resample('1M').mean()
 
-ds.index = ds.index.year 
+# ds.index = ds.index.year 
 
-fig, ax = plt.subplots(
-    sharex = True,
-    dpi = 300, 
-    nrows = 6, 
-    figsize = (12, 18), 
-    )
-
-
-cols = ['ratio', 'vp', 'mer_perp', 'K', 
-        'gr', 'gamma']
-
-limits = [
-    
-    ]
-for i, col in enumerate(cols):
-    
-    ax[i].bar(ds.index, ds[col], width = 5)
+def plot():
+    fig, ax = plt.subplots(
+        sharex = True,
+        dpi = 300, 
+        nrows = 6, 
+        figsize = (12, 18), 
+        )
     
     
+    cols = ['ratio', 'vp', 'mer_perp', 'K', 
+            'gr', 'gamma']
+    
+    limits = [
+        
+        ]
+    for i, col in enumerate(cols):
+        
+        ax[i].bar(ds.index, ds[col], width = 1)
+    
+
+ds['ratio'].plot()
