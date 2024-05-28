5ed03ad1691c5a3246cea81c5fdf56f6078df826
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2024-05-28 14:19:05 +0900

    correcting labels positions

--- heatmaps/plot_annual_hourly.py
+++ heatmaps/plot_annual_hourly.py
@@ -4,21 +4,40 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import pandas as pd 
 import datetime as dt
+import GEO as gg 
+
+b.config_labels(fontsize = 22)
+
 PATH_INDEX =  'database/indices/omni_pro.txt'
 
-def plot_terminator(ax, df):
+def plot_terminator(ax, df, lon):
     
     ax.plot(df.index, df['dusk'], lw = 2, color = 'w')
-    ax.axhline(27, lw = 2, color = 'w')
     
+    dn = df.index[0]
     
+    midnight = gg.local_midnight(dn, lon, delta_day = None)
+    midnight = round(b.dn2float(midnight))
+    ax.axhline(midnight, lw = 2, color = 'w')
+
     ax.text(
-        0.01, 0.62, 
-        'Local midnight', 
+        0.01, 0.05, 
+        'Solar terminator (300 km)', 
         color = 'w',
         transform = ax.transAxes
         )
-
+    
+    ax.text(
+         dn, midnight + 0.5, 
+        'Local midnight', 
+        color = 'w',
+        transform = ax.transData
+        )
+    
+    ax.plot(df.index, df['dusk'] + 2,
+            linestyle = '--', 
+            lw = 2, color = 'w')
+    
 def plot_seasonal_hourly(
         ax,
         df2, 
@@ -75,12 +94,7 @@ def plot_seasonal_hourly(
         
    
     
-    ax.text(
-        0.01, 0.05, 
-        'Sunset (300 km)', 
-        color = 'w',
-        transform = ax.transAxes
-        )
+    print(yticks)
     
     yticks = np.arange(yticks[0], yticks[-1] + 2, 2)
  
@@ -90,18 +104,20 @@ def plot_seasonal_hourly(
            yticks = yticks,
            xticks = xticks,
            xlim = [xticks[0], xticks[-1]],
-           xticklabels = xticklabels,
-         #  xlabel = xlabel,
-          # ylabel = ylabel
+           # xlim = [xticks[0], xticks[-1]],
+            xticklabels = xticklabels,
+           # xlabel = xlabel,
+           ylabel = ylabel
        )
     return ax
 
 
-def plot_f107(ax, limit = 84.4):
+def plot_f107(ax, limit = 84.33):
     
     df = b.load(PATH_INDEX)
 
-    df["f107a"] = df["f107"].rolling(window = 81).mean(center = True)
+    df["f107a"] = df["f107"].rolling(
+        window = 81).mean(center = True)
     
     df = b.sel_dates(
         df, 
@@ -111,25 +127,33 @@ def plot_f107(ax, limit = 84.4):
 
 
     ax.plot( df["f107"])
-    ax.plot( df["f107a"], lw = 3, color = 'cornflowerblue')
+    ax.plot( 
+        df["f107a"], 
+        lw = 3, color = 'cornflowerblue', 
+        label = '81 days average'
+        )
         
     ax.set(
         ylabel = '$F_{10.7}$ (sfu)', 
         ylim = [50, 250],
-        yticks = np.arange(50, 350, 50)
+        yticks = np.arange(60, 350, 50),
+        xlabel = 'Years'
         )
 
 
     ax.axhline(
         limit, 
         lw = 2, 
-        color = 'r'
+        color = 'r', 
+        label = f'{limit} sfu'
         )
+    
+    ax.legend(loc = 'upper right')
         
 
 
 
-def plot_annual_hourly(df):
+def plot_annual_hourly(df, sector = -50):
 
     df2 = pb.hourly_annual_distribution(df, step = 1)
     
@@ -143,35 +167,43 @@ def plot_annual_hourly(df):
     
     
     plt.subplots_adjust(hspace = 0.1)
-    plot_f107(ax[0])
+    plot_f107(ax[1])
     
     
-    fig = plot_seasonal_hourly(
-        ax[1],
+    plot_seasonal_hourly(
+        ax[0],
         df2, 
-        df,
         cmap = 'jet',
         fontsize = 35, 
         translate = True,
-        heatmap = True
+        heatmap = False
         )
+    
+    plot_terminator(ax[0], df, sector)
+    
     
     b.plot_letters(
         ax, y = 0.85, x = 0.03,
-        num2white = 1)
+        num2white = [0]
+        )
     
     return fig
 
 
-# ds = b.load('events_class2')
+def main():
 
-# df = ds.loc[(ds['lon'] == -50) & (ds.index.year < 2023)] 
-
-# fig = plot_annual_hourly(df)
-
-# FigureName = 'hourly_annual_variation'
-  
-# fig.savefig(
-#         b.LATEX(FigureName, folder = 'paper2'),
-#         dpi = 400
-#         )
+    ds = b.load('events_class2')
+    
+    df = ds.loc[(ds['lon'] == -50) & (ds.index.year < 2023)] 
+    
+    fig = plot_annual_hourly(df)
+    
+    FigureName = 'hourly_annual_variation'
+      
+    # fig.savefig(
+    #         b.LATEX(FigureName, folder = 'paper2'),
+    #         dpi = 400
+    #         ) 
+    
+    
+main()
