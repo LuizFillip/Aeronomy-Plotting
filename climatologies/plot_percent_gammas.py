aa3c87c6c4ac11d7f4d27a5e5dedd75b077f231d
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2024-06-06 14:51:13 +0900

    updates

--- climatologies/plot_percent_gammas.py
+++ climatologies/plot_percent_gammas.py
@@ -3,52 +3,113 @@ import matplotlib.pyplot as plt
 import base as b 
 import core as c 
 import RayleighTaylor as rt 
-
+import pandas as pd 
 
 b.config_labels()
 
+args_scatter = dict(s = 30, alpha = 0.4)
 
-def plot_percent_gamma_weigths(df, cols):
+
+def plot_percent_gamma_weigths(
+        df, cols, 
+        translate = False, 
+        freq = '27D'
+        ):
     
     fig, ax = plt.subplots(
-        figsize = (16, 6), 
-        dpi = 300
+        figsize = (16, 8), 
+        dpi = 300, 
+        sharex = True, 
+        nrows = 1
         )
+    
+    plt.subplots_adjust(hspace = 0.05)
+    
+    if translate:
+        ylabel = 'Contribution on $\gamma_{RT}$'
+        xlabel = 'Years'
+        sum_label = 'Sum of all terms'
+    else:
+        ylabel = 'Contribuição em $\gamma_{RT}$'
+        xlabel = 'Anos'
+        sum_label = 'Soma de todos os termos'
 
     lb = rt.EquationsFT()
-
-    df = df.resample('1M').mean()
-
-    names = [lb.gravity, lb.drift, lb.winds]
-
+   
+    ds = df.resample(freq).mean()
+    
+    names = [lb.winds, lb.drift,  lb.gravity]
+    colors = ['blue', 'red', 'green']
+    markers = ['^', 's', 'o']
+    out = []
+    
     for i, col in enumerate(cols[1:]):
-       
-        ax.plot((df[col] / df['gamma']),
-                label = names[i], lw = 2
+        
+        percent = (ds[col] / ds['gamma'])
+        # ax[0].plot(
+        #     ds[col],
+        #     label = names[i], 
+        #     lw = 1,
+        #     marker = markers[i],
+        #     color = colors[i]
+        #         )
+        
+        ax.plot(
+            percent,
+            lw = 1,
+            label = names[i],
+            marker = markers[i],
+            color = colors[i]
                 )
-       
+        out.append(percent)
+        
+    su = pd.concat(out, axis = 1).sum(axis = 1).round()
+    
+    ax.scatter(su.index, su, s = 10, c = 'k')
+    
+    ax.text(
+        0.8, 0.9, 
+        sum_label, 
+        transform = ax.transAxes
+        )
+    
     ax.legend(
         ncol = 3, 
-        bbox_to_anchor = (0.5, 1.25),
+        bbox_to_anchor = (0.5, 1.2),
         columnspacing = 0.3,
         loc = "upper center"      
         )
-
+    
+   
+       
     ax.set(
-        ylim = [-0.5, 1],
+        ylim = [-0.55, 1.2],
         xlim = [df.index[0], df.index[-1]],
-        ylabel = 'Contribuição em $\gamma_{RT}$', 
-        xlabel = 'Anos'
+        xlabel = xlabel, 
+        ylabel = ylabel
         )
     
+    # ax[0].set(
+    #     ylabel = lb.ylabel, 
+    #     ylim = [-0.5, 3]
+    #     )
+    
     ax.axhline(0, linestyle = '--')
+    
+    # b.plot_letters(
+    #     ax, 
+    #     y = 0.85, 
+    #     x = 0.02, 
+    #     num2white = None
+    #     )
+    
     plt.show()
     
     return fig
 
 
 def main():
-    cols = ['gamma', 'gravity', 'drift', 'winds']
+    cols = ['gamma',  'winds', 'drift', 'gravity']
     
     df  = c.gamma(
             site = 'saa', 
@@ -58,12 +119,17 @@ def main():
             gamma_cols = cols
             )
     
-    
-    fig = plot_percent_gamma_weigths(df, cols)
-    
+    df = df.loc[df.index.year < 2023].dropna()
+
+    fig = plot_percent_gamma_weigths(
+        df, cols, translate = True)
+        
     FigureName = 'weight_in_each_term'
     
     fig.savefig(
         b.LATEX(FigureName, folder = 'climatology'),
-        dpi = 400
+        dpi = 300
         )
+    
+    
+# main()
