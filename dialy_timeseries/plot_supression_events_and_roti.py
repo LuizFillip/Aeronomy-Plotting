50eb8cdd522ce179e277d4a088236c2679244152
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2024-06-11 17:05:18 +0900

    new layout and auxiliar functions to plot

--- dialy_timeseries/plot_supression_events_and_roti.py
+++ dialy_timeseries/plot_supression_events_and_roti.py
@@ -2,73 +2,132 @@ import datetime as dt
 import core as c 
 import matplotlib.pyplot as plt
 import base as b 
+import PlasmaBubbles as pb 
+import pandas as pd 
+import numpy as np 
 
 PATH_EPB = 'database/epbs/longs/'
 PATH_DST = 'database/indices/omni_hourly.txt'
 
 b.config_labels()
 
-def plot_dialy_roti_and_dst():
+
+
+def plot_roti(ax, start, end, root = 'E:\\'):
+    
+    out = []
+    for dn in pd.date_range(start, end):
+        
+        path = pb.path_roti(dn, root = root)
+    
+        out.append(
+            pb.load_filter(path, remove_noise = True))
+    
+    
+    df = pd.concat(out)
+    df = df.loc[df['sts'].isin(['salu', 'pbjp'])]
+    
+    ds = b.sel_dates(df, start, end)
+    
+    ax.scatter(
+        ds.index, ds['roti'], 
+        c = 'k', s = 5, 
+        alpha = 0.6)
+    ax.set(
+        xlim = [ds.index[0], ds.index[-1]],
+        ylim = [0, 6], 
+        yticks = np.arange(0, 6, 1)
+        )
+    
+    return None 
+
+
+def plot_dst(ax, dn, start, end):
+    
+    df = b.load(PATH_DST)
+    df = b.sel_dates(df, start, end)
+    
+    ax1 = ax.twinx()
+    
+    ax1.plot(df['dst'], color = 'r')
+    
+    b.change_axes_color(
+            ax1, 
+            color = 'red',
+            axis = "y", 
+            position = "right"
+            )
+    
+    start = dn + dt.timedelta(hours = 21)
+    
+    ax.axvspan(
+        start, 
+        start + dt.timedelta(hours = 12), 
+        ymin = 0, 
+        ymax = 1,
+        alpha = 0.2, 
+        color = 'gray'
+        )
+    
+    ax1.set(
+        ylim = [-300, 100], 
+        yticks = np.arange(-300, 100, 100), 
+        )
+    ax1.axhline(0, linestyle = '--', color = 'red')
+    
+    return None
+
+def plot_dialy_roti_and_dst(
+        days,
+        translate = False, 
+        fontsize = 30, 
+        nrows = 4
+        
+        ):
+    
+    if translate:
+        xlabel = 'Days'
+    else:
+        xlabel = 'Dias'
+    
     
     fig, ax = plt.subplots(
-        nrows = 5, 
+        nrows = nrows, 
         dpi = 300, 
         figsize = (12, 10),
         sharey = True
         )
     
     
-    plt.subplots_adjust(hspace = 0.8)
+    plt.subplots_adjust(hspace = 0.25)
     
-    df = c.atypical_frame(lon = -50, kind = 0, days = 3)
-    days = df.loc[df['dst'] < -90]
-    
-    # print(days)
-    for row, dn in enumerate(days.index[:5]):
+   
+    for i, dn in enumerate(days[:nrows]):
         
         start = dn - dt.timedelta(days = 2)
         end = dn + dt.timedelta(days = 4)
         
-        ds = b.sel_dates(
-            b.load(f'{PATH_EPB}{dn.year}'), 
-            start, end)
+        plot_roti(ax[i], start, end, root = 'E:\\')
+       
+        plot_dst(ax[i], dn, start, end)
         
-        df = b.sel_dates(
-            b.load(PATH_DST), 
-            start, end
+        b.format_days_axes(ax[i])
+        
+        month = b.monthToNum(dn.month, language = 'pt')
+        
+        l = b.chars()[i]
+        
+        s = f'({l}) {month}, {dn.year}'
+        
+        ax[i].text(
+            0.02, 0.8, s, 
+            transform = ax[i].transAxes
             )
-        ax[row].plot(ds['-50'])
-        ax1 = ax[row].twinx()
-        
-        ax1.plot(df['dst'], color = 'r')
-        b.change_axes_color(
-                ax1, 
-                color = 'red',
-                axis = "y", 
-                position = "right"
-                )
-        
-        start = dn + dt.timedelta(hours = 21)
-        ax[row].axvspan(
-            start, 
-            start + dt.timedelta(hours = 12), 
-            ymin = 0, 
-            ymax = 1,
-            alpha = 0.2, 
-            color = 'gray'
-            )
-        
-        b.format_days_axes(ax[row])
-        
-        ax1.set(ylim = [-250, 80], 
-                title = dn.strftime('%B, %Y'))
-        
-        ax[row].set(ylim = [0, 3])
-    ax[-1].set(xlabel = 'Days')
     
-    fontsize = 30
+    ax[-1].set(xlabel = xlabel)
+    
     fig.text(
-        0.03, 0.35, 
+        0.04, 0.36, 
         'ROTI (TECU/min)', 
         fontsize = fontsize, 
         rotation = 'vertical'
@@ -83,10 +142,15 @@ def plot_dialy_roti_and_dst():
         )
     return fig 
 
-fig = plot_dialy_roti_and_dst()
+df = c.atypical_frame(lon = -50, kind = 0, days = 3)
+ 
+days = df.loc[df['dst'] < -90].index
+ 
+fig = plot_dialy_roti_and_dst(days)
 
 FigureName = 'supression_events'
-# fig.savefig(
-#       b.LATEX(FigureName, 'timeseries'),
-#       dpi = 400
-#       )
+
+fig.savefig(
+      b.LATEX(FigureName, 'timeseries'),
+      dpi = 400
+      )
