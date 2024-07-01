eae745390212558ae6e90a1bddc9c92a1829df48
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2024-07-01 19:36:03 +0900

    new routine for build seasonal count

--- climatologies/plot_GRT_in_bars.py
+++ climatologies/plot_GRT_in_bars.py
@@ -3,36 +3,8 @@ import pandas as pd
 import base as b 
 import datetime as dt 
 import matplotlib.pyplot as plt 
+import PlasmaBubbles as pb 
 
-
-
-# ds.index = ds.index.year 
-
-def plot():
-    fig, ax = plt.subplots(
-        sharex = True,
-        dpi = 300, 
-        nrows = 6, 
-        figsize = (12, 18), 
-        )
-    
-    
-    cols = ['ratio', 'vp', 'mer_perp', 'K', 
-            'gr', 'gamma']
-    
-    limits = [
-        
-        ]
-    for i, col in enumerate(cols):
-        
-        ax[i].bar(ds.index, ds[col], width = 1)
-    
-
-# ds['ratio']
-# 
-# df = ds.groupby(pd.Grouper(freq='M')).mean()
-
-# df['gamma'].plot()
 
 
 def simple_avg(df):
@@ -52,109 +24,110 @@ def simple_avg(df):
         out['mer'].append(su['mer'].mean())
         
         
-    df = pd.DataFrame(out, index = range(1, 13, 1))
+    return pd.DataFrame(out, index = range(1, 13, 1))
 
-# df['gamma'].plot(kind = 'bar')
-
-def seasonal_mean(df, parameter = 'gamma'):
-    
+def run_by_season(df, year, parameter = 'gamma'):
     df['doy'] = df.index.day_of_year
     
     names = ['march', 'june', 'september', 'december']
+    out = {name: [] for name in names}
     
+    for season in names:
+        ss = c.SeasonsSplit(
+            df, season, translate = True)
+        
+        if parameter == 'epb':
+            out[season].append(
+                ss.sel_season[parameter].sum())
+        else:
+            out[season].append(
+                ss.sel_season[parameter].mean())
+    return pd.DataFrame(out, index = [year])
+     
+def seasonal_by_year(df, parameter = 'gamma'):
+    
+    
+
     out_year = []
-    for year in range(2013, 2023, 1):
+    for year in range(2013, 2024, 1):
             
         df1 = df.loc[df.index.year == year]
         
-        out = {name: [] for name in names}
-        
-        for season in names:
-            ss = c.SeasonsSplit(
-                df1, season, translate = True)
-            
-            if parameter == 'epb':
-                out[season].append(
-                    ss.sel_season[parameter].sum())
-            else:
-                out[season].append(
-                    ss.sel_season[parameter].mean())
-        
-        out_year.append(pd.DataFrame(out, index = [year]))
+        out_year.append(
+            run_by_season(
+                df1, year, parameter = 'gamma')
+            )
         
     return pd.concat(out_year)
 
 PATH_GAMMA = 'database/gamma/p1_saa.txt'
 
-df = b.load(PATH_GAMMA)
+# df = b.load(PATH_GAMMA)
 
-df = df.loc[(df.index.time == dt.time(22, 0)) ]
+# df = df.loc[(df.index.time == dt.time(22, 0)) ]
 
-df = c.load_results()
-df['K'] = df['K'] * 1e5 
+# df = c.load_results()
+# df['K'] = df['K'] * 1e5 
 
-ds = df.resample('1M').mean()
+# ds = df.resample('1M').mean()
 
-b.config_labels()
 
-def plot_seasonal_count():
+
+def plot_seasonal_count(ds):
     
- 
-    names = ['epb']
+    b.config_labels()
     
-    for i, name in enumerate(names):
-        
-        ds = seasonal_mean(df, name)
-        
-        ds.plot(
-            ax= ax[i], 
-            kind = 'bar', 
-            legend = False
-            )
-        
-        ax[i].set(ylabel = name)
+    fig, ax = plt.subplots(
+                figsize = (12, 8),
+                dpi = 300,
+                sharex = True,
+                nrows = 2
+                )
+            
+    plt.subplots_adjust(hspace = 0.1)
+      
+    ds = seasonal_mean(ds, parameter = 'epb')
+   
+    ds.plot(
+        ax= ax[0], 
+        kind = 'bar', 
+        legend = False
+        ) 
+   
+   
+    df2 = df.loc[df['kp'] > 3]
+   
+    ds = seasonal_mean(df2, parameter = 'epb')
+   
+    ds.plot(
+        ax= ax[1], 
+        kind = 'bar', 
+        legend = False
+        ) 
+   
+    ax[0].legend(
+         bbox_to_anchor = (0.5, 1.3), 
+         ncol = 4, 
+         loc = 'upper center'
+         )
+     
+    ax[-1].set(xlabel = 'Years')
+     
+    plt.xticks(rotation = 0)
         
    
-    
     return fig 
 
-fig, ax = plt.subplots(
-       figsize = (12, 8),
-       dpi = 300,
-       sharex = True,
-       nrows = 2
-       )
-   
-   
-plt.subplots_adjust(hspace = 0.1)
+df = pb.sel_typing(
+    b.load('events_class2'), 
+    typing = 'sunset', indexes = True)
 
-df1 = df.loc[df['kp'] <= 3]
+ds = df.loc[df['dst'] >= -30]
 
-ds = seasonal_mean(df1, parameter = 'epb')
+# ds = df.loc[df['kp'] <= 3]
 
-ds.plot(
-    ax= ax[0], 
-    kind = 'bar', 
-    legend = False
-    ) 
+ds = ds.rename(columns = {-50: 'epb'})
 
+year = 'min'
 
-df2 = df.loc[df['kp'] > 3]
-
-ds = seasonal_mean(df2, parameter = 'epb')
-
-ds.plot(
-    ax= ax[1], 
-    kind = 'bar', 
-    legend = False
-    ) 
-
-ax[0].legend(
-     bbox_to_anchor = (0.5, 1.3), 
-     ncol = 4, 
-     loc = 'upper center'
-     )
- 
-ax[-1].set(xlabel = 'Years')
- 
-plt.xticks(rotation = 0)
+run_by_season(df, year, parameter = 'gamma')
