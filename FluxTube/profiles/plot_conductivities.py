764ab6065a86642d2b50d9966bfc90f03ab01141
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2024-07-03 17:58:43 +0900

    new labels and adjusts

--- FluxTube/profiles/plot_conductivities.py
+++ FluxTube/profiles/plot_conductivities.py
@@ -4,14 +4,33 @@ import numpy as np
 import plotting as pl 
 import pandas as pd
 
-b.config_labels(fontsize = 25)
+b.config_labels(fontsize = 35)
 
 
-lb = b.Labels().infos
+lb = b.labels
 
 
 names = ['Norte', 'Sul', 'Total']
 
+def total_ratio(ax, ds):
+    
+    for i, col in enumerate(['north', 'south']):
+       
+        region_E = ds.loc[ds['hem'] == col, 'E']
+        region_F = ds.loc[ds['hem'] == col, 'F']
+        
+        
+        ratio = region_F / (region_E + region_F) 
+        
+        ax.plot(b.smooth2(ratio, 2), ratio.index, lw = 2)
+
+    ax.set(
+        xlabel = b.y_label('ratio'),
+        xlim = [0.5, 1.2],
+        xticks = [0.6, 0.8, 1]
+        )
+    
+    return None
 
 def plot_height_prf(ax, ds, region = 'E'):
     
@@ -21,70 +40,74 @@ def plot_height_prf(ax, ds, region = 'E'):
         number = 2
         
     out = []
+   
     for i, col in enumerate(['north', 'south']):
         name = names[i]
         df = ds.loc[ds['hem'] == col, region]
+        
         out.append(df)
-        ax.plot(
-            b.smooth2(df, number), 
+        # vars()[col].append(df)
+        
+        ax.plot(b.smooth2(df, number), 
                 df.index, 
-                label = name)
+                label = name, lw = 2)
         
-        ax.set(xlabel = b.y_label("S"+ region))
+        ax.set(xlabel = b.y_label("S" + region))
         
-    total = pd.concat(
-        out, axis = 1
-        ).dropna().sum(axis = 1)
-
+    total = pd.concat(out, axis = 1).dropna().sum(axis = 1)
+    
+    
     ax.plot(
         b.smooth2(total, number), 
         total.index, 
-        label = 'Total')
+        label = 'Total', lw = 2)
     
 
     return total
 
-def total_ratio(ax, region_E, region_F):
 
-        
-    ratio = region_F / (region_E + region_F) 
-    
-    ax.plot(b.smooth2(ratio, 2), 
-            ratio.index, 
-            lw = 1, label = 'Total', color = 'k')
-
-    ax.set(
-        xlabel = b.y_label('ratio'),
-        xlim = [0.5, 1.2],
-        )
-    
-    
-    ax.axvline(1, linestyle = ':')
 
 
 def plot_conductivities(df):
     
     fig, ax = plt.subplots(
         ncols = 3, 
-        sharey= True,
+        sharey = True,
         dpi = 300, 
-        figsize = (14, 10))
+        figsize = (16, 10)
+        )
     
     
     plt.subplots_adjust(wspace = 0.1, hspace = 0.1)
     
     region_E = plot_height_prf(ax[0], df, region = 'E')
     region_F = plot_height_prf(ax[1], df, region = 'F')
-    total_ratio(ax[2], region_E, region_F)
+    total_ratio(ax[2], df)
     
-    ax[0].set(ylabel = 'Altura de Apex (km)', 
-              xticks = np.arange(0, 8, 1), 
-              ylim = [100, 500]
-              )
+    ratio = region_F / (region_E + region_F) 
+    
+    ax[2].plot(b.smooth2(ratio, 2), ratio.index, lw = 2)
+    
+    ax[0].set(
+        ylabel = 'Altura de Apex (km)', 
+        xlim = [-2, 8],
+        xticks = np.arange(0, 8, 2), 
+        ylim = [100, 500]
+        )
+
+    ax[0].axvline(0, linestyle = '--')
         
-    ax[2].legend(ncol = 1,loc = "lower center")
-    ax[1].legend(ncol = 1,loc = "lower right")
-    b.plot_letters(ax, y = 1.03, x = 0.01)
+    ax[1].legend(
+        ncol = 3, 
+        bbox_to_anchor = (.5, 1.15),
+        loc = "upper center"
+        )
+    
+    ax[2].axvline(1, linestyle = ':')
+    
+    ax[1].set(xticks = np.arange(0, 160, 40))
+   
+    b.plot_letters(ax, y = 0.92, x = 0.06, fontsize = 40)
     
     return fig
 
@@ -99,3 +122,5 @@ def main():
         b.LATEX(FigureName, folder = 'profiles'),
         dpi = 400
         )
+
+# main()
