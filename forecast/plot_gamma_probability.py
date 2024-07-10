f17ff88872ecbd8351e6b2cc5eb074430bad608d
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2024-07-10 13:30:09 +0900

    new configuration to estimate EPBs

--- forecast/plot_gamma_probability.py
+++ forecast/plot_gamma_probability.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt 
 import core as c 
 import base as b 
+import datetime as dt 
 
 b.config_labels()
 
@@ -10,15 +11,16 @@ def plot_gamma_predict_epbs(df):
         dpi = 300, 
         nrows = 3,
         sharex = True,
-        figsize = (14, 12)
+        figsize = (16, 14)
         )
     
     plt.subplots_adjust(hspace = 0.1)
     
-    ax[0].plot(df['gamma'], lw = 2)
+    ax[0].plot(df['gamma'], lw = 1.5)
     
-    ax[1].plot(df['predict'] *100, lw = 2, color = 'k')
+    ax[1].plot(df['predict'] * 100, lw = 1.5, color = 'k')
     
+    ax[1].axhline(50, linestyle = ':')
     
     ax[2].plot(
         df['epb'], 
@@ -40,7 +42,11 @@ def plot_gamma_predict_epbs(df):
         label = 'Estimadas'
         )
     
-    ax[2].legend(ncol = 2, loc = 'upper center')
+    ax[2].legend(
+        bbox_to_anchor = (0.5, 1.05), 
+        ncol = 2, 
+        loc = 'upper center'
+        )
     
     ax[1].set(
         ylabel = 'Probabilidade (\%)', 
@@ -54,7 +60,7 @@ def plot_gamma_predict_epbs(df):
         )
     
     ax[2].set(
-        ylabel = 'Ocorrência de EPBs', 
+        ylabel = 'Ocorrência', 
         ylim = [-0.2, 1.4], 
         xlim = [df.index[0], df.index[-1]],
         yticks = [0, 1]
@@ -67,22 +73,26 @@ def plot_gamma_predict_epbs(df):
         ax[1].axhline(line * 100, lw = 0.5, linestyle = '--') 
         ax[2].axhline(line, lw = 0.5, linestyle = '--')
     
-    b.plot_letters(ax, y = 0.85, x = 0.02)
+    b.plot_letters(ax, y = 0.85, x = 0.02, fontsize = 40)
     
     return fig 
 
 
 def main():
-    
+    time = dt.datetime(2023, 11, 13)
     df = c.load_results('saa', eyear = 2023)
+    df.loc[df.index > time, 'gamma'] *= 0.7
     ds = c.predict_seasons(df)
     
     fig = plot_gamma_predict_epbs(ds)
     
     FigureName = 'Estimation_2023'
     
-    fig.savefig(b.LATEX(FigureName, folder = 'climatology'))
+    fig.savefig(
+        b.LATEX(FigureName, folder = 'climatology')
+        )
     
     
-# main()
+main()
+
 
