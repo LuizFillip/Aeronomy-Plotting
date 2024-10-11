b8d128826a4fe7f104d589278cf626313382e247
LuizFillip <luizfillip@outlook.com>
2024-10-10 22:41:46 -0300

    updates

--- imager/plot_sequence_images_linear.py
+++ imager/plot_sequence_images_linear.py
@@ -7,11 +7,30 @@ import base as b
 
 b.config_labels(fontsize = 25)
 
+def text_BJL(ax, i, fontsize):
+    ax.text(
+        -41 - i/2, -14, 'M', 
+        color = 'white', 
+        fontsize = fontsize + 5
+        )
+    
+    
+    if i >= 4:
+        
+        ax.text(
+            -41, -10, 'B', 
+            color = 'white', 
+            fontsize = fontsize + 5
+            )
+    
+    return None 
+
 def plot_linear_images_sequence(
         times, 
         site, 
-        areap, 
-        fontsize = 25
+        areap = 1536, 
+        fontsize = 25,
+        ext = 'I'
         ):
 
     fig, ax = plt.subplots(
@@ -45,6 +64,7 @@ def plot_linear_images_sequence(
         title = file_time.strftime(f'({l}) %Hh%M UT')
         ax.set_title(title, fontsize = fontsize)
         
+        
         if i != 6:
             
             ax.set(
@@ -62,16 +82,33 @@ def plot_linear_images_sequence(
                 'Latitude (°)', 
                 fontsize = fontsize
                 )
+        
+        if site == 'CA':
+            ax.text(
+                -40, -8, 'B', 
+                color = 'white', 
+                fontsize = fontsize + 5
+                )
+        else:
+            text_BJL(ax, i, fontsize)
+               
     
     infos = im.sites_infos(site)
     
-    fig.suptitle(infos.name, y = 0.95)
+    fig.suptitle(f'({ext}) {infos.name}', y = 0.95)
     return fig
 
-alt_ag, areap, site =  250, 1536, 'BJL'
 
 start = dt.datetime(2022, 7, 25, 1, 20)
 
 times = pd.date_range(start, freq  = '30min', periods = 12)
 
-fig = plot_linear_images_sequence(times, site, areap)
+fig_1 = plot_linear_images_sequence(times, site = 'CA', ext = 'I')
+fig_2 = plot_linear_images_sequence(times, site = 'BJL', ext = 'II')
+
+fig = b.join_images(fig_1, fig_2)
+
+# fig.savefig(
+#     b.LATEX(FigureName, folder = 'products'),
+#     dpi = 400
+#     )
