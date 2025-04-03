2a9325d92175a2f0bc100fe01b24b9296e9d2453
LuizFillip <luizfillip@outlook.com>
2025-04-03 10:17:27 -0300

    new layout for plot fixed frequencies

---
+++ ionosonde/heights/plot_height_different_sites.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import datetime as dt 
+import numpy as np 
+import digisonde as dg 
+import base as b 
+import plotting as pl
+
+ # file =  dg.dn2fn(dn, site)
+ 
+ # df = dg.IonoChar(
+ #     file, cols, 
+ #     sum_from = None
+ #     ).heights.interpolate()
+ 
+ # df = df.between_time('17:00', '21:00')
+ 
+ # for x in df.columns:
+ #     vls = b.filter_frequencies(
+ #             df[x], 
+ #             high_period = 5, 
+ #             low_period = 1, 
+ #             fs = 6, 
+ #             order = 1
+ #             )
+ #     print(len(vls), len(df))
+ #     ax[i].plot(df.index, vls)
--- ionosonde/heights/plot_heights_fixes_frequency.py
+++ ionosonde/heights/plot_heights_fixes_frequency.py
@@ -2,14 +2,14 @@ import matplotlib.pyplot as plt
 import digisonde as dg 
 import datetime as dt 
 import base as b 
-
-sites = ['FZA0M', 'SAA0K', 'BVJ03', 'BLJ03'] #, 'CAJ2M']
+import plotting as pl 
+import numpy as np 
 
 
 def plot_height_fixes_for_multi_sites(
         sites,
         translate = True,
-        cols = list(range(4, 10, 1)),
+        cols = list(range(4, 11, 1)),
         fontsize = 35
 
         ):
@@ -19,12 +19,12 @@ def plot_height_fixes_for_multi_sites(
         dpi = 300,
         sharey = True, 
         sharex = True,
-        figsize = (16, 10)
+        figsize = (16, 12)
         )
     
     plt.subplots_adjust(hspace = 0.1)
     
-    dn = dt.datetime(2023, 10, 14)
+    ref_day = dt.datetime(2015, 12, 19)
     
     if translate:
        ylabel = 'Height (km)'
@@ -37,44 +37,31 @@ def plot_height_fixes_for_multi_sites(
     
     for i, site in enumerate(sites):
         
-        # df = dg.join_iono_days(
-        #         site, 
-        #         dn,
-        #         cols = cols,
-        #         parameter = 'heights'
-        #         )
+        df = dg.join_iono_days(
+                site, 
+                ref_day,
+                cols = cols,
+                parameter = 'heights'
+                )
         
-        file =  dg.dn2fn(dn, site)
+        ax[i].plot(df) 
         
-        df = dg.IonoChar(
-            file, cols, 
-            sum_from = None
-            ).heights.interpolate()
+        ax[0].legend(
+            cols, 
+            ncol = len(cols), 
+            title = title,
+            bbox_to_anchor = (0.5, 1.9), 
+            loc = 'upper center', 
+            columnspacing = 0.7
+            )
         
-        df = df.between_time('17:00', '21:00')
+        pl.plot_terminators(ax[i], df, site)
         
-        # pl.plot_terminators(ax[i], df, site)
-        
-        
-        # for x in df.columns:
-        #     vls = b.filter_frequencies(
-        #             df[x], 
-        #             high_period = 5, 
-        #             low_period = 1, 
-        #             fs = 6, 
-        #             order = 1
-        #             )
-            # print(len(vls), len(df))
-            # ax[i].plot(df.index, vls)
-        ax[i].plot(df)  
-        delta = dt.timedelta(hours = 16)
-        lim = 30
+
         ax[i].set(
-            # ylim = [-lim, lim],
-            ylim = [0, 500],
-            # xlim = [
-            #     df.index[0] + delta,
-            #     df.index[-1] ]
+            ylim = [0, 600],
+            yticks = np.arange(100, 800, 200),
+            xlim = [df.index[0], df.index[-1]]
             )
         
         s = b.chars()[i]
@@ -85,16 +72,16 @@ def plot_height_fixes_for_multi_sites(
             transform = ax[i].transAxes
             )
         
-        # start = dt.datetime(2015, 12, 20, 21, 0)
+        start = dt.datetime(2015, 12, 20, 21, 0)
         
-        # ax[i].axvspan(
-        #      start, 
-        #      start + dt.timedelta(hours = 12), 
-        #      ymin = 0, 
-        #      ymax = 1,
-        #      alpha = 0.2, 
-        #      color = 'gray'
-        #      )
+        ax[i].axvspan(
+              start, 
+              start + dt.timedelta(hours = 12), 
+              ymin = 0, 
+              ymax = 1,
+              alpha = 0.2, 
+              color = 'gray'
+              )
         
     fig.text(
         0.03, 0.35, 
@@ -103,36 +90,33 @@ def plot_height_fixes_for_multi_sites(
         rotation = 'vertical'
         )
   
-    ax[0].legend(
-        cols, 
-        ncol = len(cols), 
-        title = title,
-        bbox_to_anchor = (0.5, 1.5), 
-        loc = 'upper center', 
-        columnspacing = 0.7
-        )
+    
     
     b.format_time_axes(
         ax[-1], 
-        hour_locator = 1, 
+        hour_locator = 12, 
         pad = 80, 
         translate = translate, 
-         # = '%d/%m/%y'
         )
         
     return fig
 
 def main():
+    sites = [ 'SAA0K', 'BVJ03', 'FZA0M', 'CAJ2M']
+
     fig = plot_height_fixes_for_multi_sites(
         sites,
         translate = True
         
         )
     
+    path_to_save = 'G:\\My Drive\\Papers\\Paper 2\\Geomagnetic control on EPBs\\June-2024-latex-templates\\'
+    
     FigureName = 'fixed_heights_sites'
     
-    # fig.savefig(
-    #       b.LATEX(FigureName, 'Iono'),
-    #       dpi = 400)
+    fig.savefig(
+          path_to_save + FigureName,
+          dpi = 400
+          )
 
 main()
