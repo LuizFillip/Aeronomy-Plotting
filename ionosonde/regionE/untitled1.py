22d69cb23f5865b3d278da5ac2982afc339d95d6
LuizFillip <luizfillip@outlook.com>
2025-12-04 22:18:58 -0300

    plot profilegram

--- ionosonde/regionE/untitled1.py
+++ ionosonde/regionE/untitled1.py
@@ -1,10 +1,10 @@
 import digisonde as dg 
 import matplotlib.pyplot as plt 
 import pandas as pd 
-
+import numpy as np 
 infile = 'plotting/ionosonde/regionE/profiles'
 df = dg.Profilegram(infile) 
-df["ne"] = (1.24e4 * df["freq"]**2) * 1e6
+df["ne"] = np.log10((1.24e4 * df["freq"]**2) * 1e6)
 
 df['time'] = df.index.hour + df.index.minute/60
 
@@ -18,9 +18,8 @@ p = 'ne'
 
 
 # ds = ds.loc[ds.alt == alt]
-ds = ds.loc[(ds.day == 13) & (ds.alt < 200)]
+ds = ds.loc[(ds.day == 16) & (ds.alt < 200)]
 
-dss = pd.pivot_table(ds, columns = 'time', index = 'alt', values = p ).interpolate()
 
 fig, ax = plt.subplots(
     nrows = 2, 
@@ -28,14 +27,23 @@ fig, ax = plt.subplots(
     sharex = True
     )
 
-img = ax[0].contourf(
-    dss.columns, 
-    dss.index, 
-    dss.values, 
-    cmap = 'jet'
-    )
+def plot(ax):
+    dss = pd.pivot_table(
+        ds, 
+        columns = 'time',
+        index = 'alt', 
+        values = p ).interpolate()
 
-plt.colorbar(img)
+    img = ax[0].contourf(
+        dss.columns, 
+        dss.index, 
+        dss.values, 
+        cmap = 'jet'
+        )
+
+    plt.colorbar(img)
+    
+    
 ds = df.loc[(df.day == 20) & (df.alt < 200)]
 
 dss = pd.pivot_table(ds, columns = 'time', index = 'alt', values = p ).interpolate()
@@ -50,3 +58,5 @@ ax[1].set(ylim = [100, 200])
 # ax.axhline(0)
 
 
+
+plt.colorbar(img)
