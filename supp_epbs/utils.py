d408dc0715487eeae3499f9fcd163ad8e4c5e540
LuizFillip <luizfillip@outlook.com>
2026-01-15 19:02:50 -0300

    adding legend by labels

--- supp_epbs/utils.py
+++ supp_epbs/utils.py
@@ -3,3 +3,32 @@ def savefig(fig, FigureName):
     path_to_save = 'G:\\Meu Drive\\Papers\\Suppression_stastistical\\June-2024-latex-templates\\'
     
     fig.savefig(path_to_save + FigureName, dpi = 300)
+    
+    
+def legend_for_sym_h(
+        ax, 
+        quiet = False, 
+        loc = 'upper right',
+        ncol = 1
+        ):
+    
+    legend_labels = {
+        'weak': '-50 $<$ SYM-H $\leq$ -30 nT',
+        'moderate': '-100 $<$ SYM-H $\leq$ -50 nT',
+        'intense': 'SYM-H $\leq$ -100 nT'
+    }
+    
+    if quiet:
+        legend_labels['quiet'] = 'SYM-H $>$ -30 nT'
+    
+    handles, labels = ax.get_legend_handles_labels()
+    
+    ax.legend(
+        handles, [legend_labels[l] for l in labels],
+        loc = loc,
+        fontsize = 20,
+        title_fontsize = 20, 
+        ncol = ncol
+    )
+    
+    return None 
