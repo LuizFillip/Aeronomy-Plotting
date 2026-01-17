6431ef917f67b5e5ffabe2ae96cebe170972bb47
LuizFillip <luizfillip@outlook.com>
2026-01-17 14:30:44 -0300

    updates

--- indices/__init__.py
+++ indices/__init__.py
@@ -2,3 +2,4 @@ from .plot_indices_long_term import plot_f107
 from .plot_one_day_indeces import *
 from .plot_high_resolution import *
 from .plot_magnetic_parameters import *
+from .plot_geomag_intervals import *
--- indices/plot_geomag_intervals.py
+++ indices/plot_geomag_intervals.py
@@ -1,40 +1,136 @@
 import base as b
 import matplotlib.pyplot as plt 
+import GEO as gg 
+import datetime as dt 
+import core as c 
 
-def test_plot(ds, sections, dn):
+b.sci_format(fontsize = 25)
+
+
+def _devtime(end, start):
+    return (end - start).total_seconds() / 3600
+
+def stormtime_spanning(ax, start, end, y = -100):
+    
+    devtime = _devtime(end, start)
+    
+    time = round(devtime, 2)
+        
+    middle = end + (start - end) / 2
+      
+    ax.annotate(
+        '', 
+        xy = (start, y), 
+        xytext = (end, y), 
+        arrowprops = dict(arrowstyle='<->')
+        )
+    time = round(time, 2)
+    
+    ax.annotate(
+        f'{time} hrs',
+        xy = (middle, y + 5), 
+        xycoords = 'data',
+        fontsize = 30.0,
+        textcoords = 'data', 
+        ha = 'center'
+        )
     
     
-    b.sci_format(fontsize = 25)
- 
-    fig, ax = plt.subplots(dpi = 200, figsize = (10, 5))
+    
+    return None   
+
+def reference_sym_hlines(ax):
+    
+    ax.axhline(0, linestyle = ':')
+    
+    for ref_line in [0, -30, -50, -100]:
+        ax.axhline(ref_line, linestyle = '--')
+        
+    return None 
+        
+def set_xaxis(ax, ds):
+    start, end = ds.index[0], ds.index[-1]
+    
+    b.axes_hour_format(
+         ax, 
+         hour_locator = 12, 
+         tz = "UTC"
+         )
+    
+    ax.set(
+        xlabel = 'Universal time', 
+        xlim = [start, end]
+        )
+
+    b.adding_dates_on_the_top(
+           ax, 
+           fmt = '%d/%m'
+           )
+    return None 
+
+  
+def plot_storm_intervals(ds, st, dn):
+     
+    fig, ax = plt.subplots(
+        dpi = 200, 
+        figsize = (12, 8)
+        )
     
     ax.plot(ds['sym'])
-    ax1 = ax.twinx()
+
+    ax.set(
+        ylim = [-150, 50],
+        ylabel = 'SYM-H (nT)',
+        title = dn.strftime('%B, %Y')
+        )
     
-    ax1.plot(ds['bz'], color = 'blue')
-    b.change_axes_color(
-            ax1, 
-            color = 'blue',
-            axis = "y", 
-            position = "right"
-            )
-    for d in sections:
-        ax.axvline(d, color = 'red')
-    
-    for ref_line in [0, -30, -50]:
-        ax.axhline(ref_line, linestyle = '--')
-  
     dusk = gg.terminator(-50, dn, float_fmt = False)
     
     ax.axvline(dusk, linestyle = '--', color = 'b', lw = 3)
-    ax.set(
-        ylabel = 'Sym/h (nT)',
-        xlabel = 'Days',
-        title = dn.strftime('%B, %Y')
+    
+    stormtime_spanning(ax, st['start'], dusk, y = 10)
+    
+    ax.axvspan(
+        st['start'], 
+        st['end'], 
+        ymin = 0, 
+        ymax = 1,
+        alpha = 0.2, 
+        color = 'tomato'
         )
-        
-    ax1.set(ylim = [-30, 30], ylabel = 'Bz (nT)')
-    b.format_days_axes(ax) 
     
-    return None
+    set_xaxis(ax, ds)
+    reference_sym_hlines(ax)
     
+    s = ds['sym'] 
+    vmin = s.min()
+    
+    time = s.idxmin(skipna = True)
+    ax.scatter(time, vmin, s = 140, marker = '^')
+    delta = dt.timedelta(days = 2)
+    xe = time + delta
+ 
+    ax.annotate(
+        f'{round(vmin)} nT',
+        xy = (time, vmin) , 
+        xycoords = 'data',
+        fontsize = 30.0,
+        textcoords = 'data', 
+        xytext = (xe, vmin - 3), 
+        arrowprops = dict(arrowstyle='->'),
+        ha = 'center'
+        )
+    
+    return fig
+    
+dn = dt.datetime(2013, 3, 17)
+
+df = c.high_omni(dn.year)
+
+df = b.range_dates(df, dn, b = 2, f = 6)
+
+st = c.find_storm_interval(df['sym'])
+
+fig = plot_storm_intervals(df, st, dn)
+
+# st['start']
