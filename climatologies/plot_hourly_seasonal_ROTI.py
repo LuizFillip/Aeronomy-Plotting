2e5d3d0d713da53ed5a1527b3d235ffb8f5b24a6
LuizFillip <luizfillip@outlook.com>
2026-01-19 20:35:31 -0300

    updates

--- climatologies/plot_hourly_seasonal_ROTI.py
+++ climatologies/plot_hourly_seasonal_ROTI.py
@@ -4,13 +4,11 @@ import base as b
 import pandas as pd 
 import datetime as dt
 
-b.config_labels()
-
 def concat_years():
     out = []
-    for year in range(2013, 2023):
+    for year in range(2013, 2024):
         
-        infile = f'database/epbs/longs/{year}'
+        infile = f'database/epbs/maximums/{year}'
         
         df = b.load(infile)
         
@@ -31,39 +29,65 @@ def concat_years():
         values = '-50'
         )
     return ds 
-infile = 'database/epbs/maximus'
+# infile = 'database/epbs/maximus'
 
-df = b.load(infile)
+# df = b.load(infile)
 
-df['time'] = b.time2float(df.index)
+# df['time'] = b.time2float(df.index)
 
     
-df['date'] = df.index.date
+# df['date'] = df.index.date
 
 
-df = df.resample('30min').mean()
+# df = df.resample('30min').mean()
 
-ds = pd.pivot_table(
-    df, 
-    columns = 'date', 
-    index = 'time', 
-    values = '-50'
-    )
+# ds = pd.pivot_table(
+#     df, 
+#     columns = 'date', 
+#     index = 'time', 
+#     values = '-50'
+#     )
 
-ds = ds.replace(np.nan, 0)
-ds = ds.interpolate(method = 'quadratic')
+# ds = ds.replace(np.nan, 0)
+# ds = ds.interpolate(method = 'quadratic')
 
-fig, ax = plt.subplots(
-    figsize = (16, 8), 
-    dpi = 300)
+# fig, ax = plt.subplots(
+#     figsize = (16, 8), 
+#     dpi = 300)
 
-img = ax.contourf(
-    ds.columns, 
-    ds.index, 
-    ds.values,
-    levels = np.arange(0, 2.2, 0.2)
-    )
+# img = ax.contourf(
+#     ds.columns, 
+#     ds.index, 
+#     ds.values,
+#     levels = np.arange(0, 2.2, 0.2)
+#     )
 
 
-plt.colorbar(img)
+# plt.colorbar(img)
+import os 
 
+infile = f'database/epbs/maximums/'
+
+
+files = os.listdir(infile)
+
+out = []
+for file in files:
+
+    df = b.load(infile + file)
+    
+    df.loc[df.between_time('23:58', '00:02').index, :] = np.nan
+    
+    df = df.interpolate()
+    
+    df = df.resample('30min').mean()
+    
+    df['time'] = b.time2float(df.index)
+    
+    df['date'] = df.index.date
+
+    out.append(df)
+    
+ds = pd.concat(out)
+
+ds.to_csv('maximums_roti')
