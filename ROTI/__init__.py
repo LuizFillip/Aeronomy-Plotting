c6c0706702b65905db3a530848e6039eb34c7ec1
LuizFillip <luizfillip@outlook.com>
2026-01-21 09:50:13 -0300

    clear attrs

--- ROTI/__init__.py
+++ ROTI/__init__.py
@@ -2,3 +2,4 @@ from .roti_base import *
 from .plot_roti_and_epb_occurrence import plot_epbs_occurrences_roti, single_plot
 from .plot_utils import *
 from .plot_roti_timeseries import plot_roti_timeseries, plot_roti_points
+from .plot_longterm_points_like import * 
---
+++ ROTI/plot_longterm_points_like.py
@@ -0,0 +1,37 @@
+import epbs as pb 
+import datetime as dt 
+
+
+def plot_roti_in_range(
+        ax, start, end, 
+        root = 'F:\\', 
+        clear = None
+        ):
+
+    ds = pb.longterm_raw_roti(start, end, root = root)
+    
+    if clear is not None :
+        delta = dt.timedelta(hours = 4)
+        ds = ds.loc[
+            ~((ds.index > clear) & 
+            (ds.index < clear + delta)  & 
+            (ds['roti'] >  0.5))]
+        
+        
+    ax.scatter(
+        ds.index, 
+        ds['roti'], 
+        c = 'k', 
+        s = 5, 
+        alpha = 0.6
+        )
+    
+    ax.set(
+        ylabel = 'ROTI\n(TECU/min)',
+        ylim = [0, 5], 
+        yticks = list(range(6)),
+        
+        )
+    
+  
+    return ds
