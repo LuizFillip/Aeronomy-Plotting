441cf2efbd35f10e33a0a744748f53da83b9a8e7
LuizFillip <luizfillip@outlook.com>
2026-01-30 21:47:28 -0300

    replacing functions

--- indices/__init__.py
+++ indices/__init__.py
@@ -1,5 +1,4 @@
 from .plot_indices_long_term import *
-from .plot_one_day_indeces import *
 from .plot_high_resolution import *
 from .plot_magnetic_parameters import *
 from .plot_geomag_intervals import *
--- indices/plot_one_day_indices.py
+++ indices/plot_one_day_indices.py
@@ -5,26 +5,8 @@ import numpy as np
 import GEO as gg 
 import plotting as pl
 import core as c 
-# PATH = 'database/indices/omni_hourly.txt'
-
-# df = c.category_and_low_indices(
-#         col_kp = 'kp_max', 
-#         col_dst = 'sym_min'
-#         )
-
-
-
-
-
-
-
-
-
 
 b.sci_format(fontsize = 20)
-
-
-
 
 def set_time_limits(ds):
     dates = np.unique(ds.index.date)
@@ -33,35 +15,13 @@ def set_time_limits(ds):
     end = dates[-1] 
     return start, end
     
- 
-def set_axes_time(ax, start, end):
-
-    b.axes_hour_format(
-         ax[-1], 
-         hour_locator = 6, 
-         tz = "UTC"
-         )
-    
-    ax[-1].set(
-        xlabel = 'Universal time', 
-        xlim = [start, end]
+def plot_IMF(ax, ds):
+    ax.plot(ds['field'], lw = 1.5)
+    ax.set(
+        ylabel = 'IMF (nT)', 
+        ylim = [0, 40]
         )
-
-    b.adding_dates_on_the_top(
-           ax[0], 
-           fmt = '%d/%m'
-           )
-    
-    b.plot_letters(
-        ax, 
-        y = 0.75, 
-        x = 0.02, 
-        fontsize = 25,
-        num2white = None
-        )
-    
     return None 
-
 
 def plot_range_day_indices(
         dn, 
@@ -72,20 +32,22 @@ def plot_range_day_indices(
         clear = None 
         ):
     
-    dusk = gg.terminator(dn
-        )
+    dusk = gg.terminator(dn)
     
     fig, ax = plt.subplots(
         dpi = 300,
         figsize = (14, 12), 
-        nrows = 5, 
+        nrows = 6, 
         sharex = True
         )
 
     plt.subplots_adjust(hspace = 0.1)
 
     ds, st = c.set_stormtime(
-        dn, before = days, after = days)
+        dn, 
+        before = days, 
+        after = days + 1
+        )
   
     start, end = set_time_limits(ds)
 
@@ -94,11 +56,13 @@ def plot_range_day_indices(
     pl.plot_magnetic_fields(ax[2], ds, ylim = 30)
     pl.plot_dst(ax[3], ds)
     pl.plot_kp_by_range(ax[3].twinx(), dn)
-    pl.plot_electric_field(ax[-1], ds)
-     
+    pl.plot_electric_field(ax[4], ds)
+    plot_IMF(ax[-1], ds)
+    
     if storm_span:
         pl.stormtime_spanning(
-            ax[3], st['start'], dusk)
+            ax[3], st['start'], dusk
+            )
     
     
     for a in ax.flat:
@@ -106,8 +70,11 @@ def plot_range_day_indices(
             st['main'],
             lw = 2, 
             color = 'purple')
+        
+        for dn in np.unique(ds.index.date):
+            a.axvline(dn, lw = 1, linestyle = '--')
     
-    set_axes_time(ax, start, end)
+    b.set_axes_time(ax, start, end)
     
     ax[0].set(title = dn.strftime('%B, %Y'))
     
@@ -123,7 +90,6 @@ dn = dt.datetime(2014, 2, 9, 21)
 dn = dt.datetime(2019, 3, 19, 21)
 dn = dt.datetime(2019, 5, 2, 21)
 dn = dt.datetime(2016, 10, 3, 21)
-dn = dt.datetime(2017, 8, 30, 21)
 dn = dt.datetime(2014, 1, 2, 21)
 dn = dt.datetime(2013, 3, 17, 21)
 dn = dt.datetime(2022, 7, 24, 21)
@@ -133,7 +99,15 @@ dn = dt.datetime(2015, 12, 20, 18)
 # dn = dt.datetime(2019, 5, 2, 21)
 # dn = dt.datetime(2019, 12, 6, 21)
 
+dn = dt.datetime(2017, 8, 30, 21)
 
 fig = plot_range_day_indices(dn, days = 3)
-    
 
+# df = c.category_and_low_indices(
+#         col_kp = 'kp_max', 
+#         col_dst = 'sym_min'
+#         )
+
+# df = df.loc[df['category'] != 'quiet']
+
+# df.loc[df['bz_mean'] < 0] 
