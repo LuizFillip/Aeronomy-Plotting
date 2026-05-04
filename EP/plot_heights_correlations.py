3fb4acb6c4d0e64b0ef1051d7044d658760de2b3
LuizFillip <luizfillip@outlook.com>
2026-05-04 18:24:38 -0300

    plot timeseries and correlations heights together

--- EP/plot_heights_correlations.py
+++ EP/plot_heights_correlations.py
@@ -2,7 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import GOES as gs
- 
+import base as b 
+
 def join_wave_nucleos(
     year=2013,
     freq="1M",
@@ -43,6 +44,7 @@ def join_wave_nucleos(
 def join_all_years(
         start, end,
         area = 0,
+        freq = "1M",
         lon_min=-70,
         lon_max=-40,
         lat_min=-10,
@@ -53,7 +55,7 @@ def join_all_years(
     for year in range(start, end + 1):
         df = gs.join_wave_nucleos(
             year=year,
-            freq = "1M",
+            freq = freq,
             ep_vls = "Ep_max",
             area = area,
             lon_min=lon_min,
@@ -77,21 +79,32 @@ def plot_heights_correlations(
         ep_vls = "Ep_max",
         area = 30,
         lon_min = -70,
-        lon_max = -50,
+        lon_max = -60,
         lat_min = -10,
         lat_max = 0,
     ):
-    df = gs.join_wave_nucleos(
-        year=year,
-        freq=freq,
-        ep_vls=ep_vls,
-        area=area,
-        lon_min=lon_min,
-        lon_max=lon_max,
-        lat_min=lat_min,
-        lat_max=lat_max,
-    )
-    
+    fig, ax = plt.subplots(
+        figsize = (10, 8),
+        dpi = 300)
+ 
+    ds = gs.join_wave_nucleos(
+         year=year,
+         freq = "1M",
+         ep_vls = "Ep_max",
+         area = area,
+         lon_min=lon_min,
+         lon_max=lon_max,
+         lat_min=lat_min,
+         lat_max=lat_max,
+     )
+    df1 = compute_corr_by_altitude(ds)
+
+    plot_altitude_corr(ax, df1, area)
+
+    # ax.set(title= f'Profiles in {lat_min} to {lat_min + 10} by area')
+
+
+    df1 
 
 
  
@@ -127,12 +140,14 @@ def compute_corr_by_altitude(df):
 
 
 
-def plot_altitude_corr(ax,df, label = 'All years'):
+def plot_altitude_corr(ax, df, label = 'All years'):
  
     ax.plot(
         df['corr'], 
         df['alt'], 
         marker="o", 
+        fillstyle = 'none',
+        markersize = 15,
         label = label
         )
     ax.axvline(0, linestyle="--", color="k", lw=1)
@@ -142,52 +157,76 @@ def plot_altitude_corr(ax,df, label = 'All years'):
         ylabel="Altitude (km)",
         xlabel="Pearson correlation",
         xlim=(-1, 1),
+        xticks = np.arange(-1, 1.5, 0.5)
     )
+    
+    corr_max = df['corr'].max()
+    
+    ax.axvline(corr_max, linestyle = ':')
      
     ax.legend()
 
-fig, ax = plt.subplots(figsize = (10, 8), dpi = 300)
-
-
-latitudes = range(-50, 10, 10)
-
-area = 0
-# for i, lat_min in enumerate(latitudes):
+def plot_seasonal_timeseries(ax, df):
     
-lat_min = -10
-# ds = join_all_years(
-#         area = area,
-
-#         lat_min=lat_min,
-#         lat_max=lat_min + 10,
-#         )
-# year = 2017
-# lat_max=lat_min + 10
-# ds = join_wave_nucleos(
-#     year=year,
-#     freq = "1M",
-#     ep_vls = "Ep_max",
-#     area = area,
-#     lon_min=-80,
-#     lon_max=-40,
-#     lat_min=lat_min,
-#     lat_max=lat_max,
-# )
-
-for area in [0, 30, 60, 90]:
-
-    ds = join_all_years(
-        start = 2012, 
-        end = 2017,
-        area = area,
-        lon_min=-80,
-        lon_max=-40,
-        lat_min=-10,
-        lat_max=0,
+    ax.plot(df['nucleos'], lw = 3, color = 'blue')
+    
+    ax1 = ax.twinx()
+    
+    ax.set(ylabel = 'Number of nucleos',
+           xlabel = 'Years')
+    
+    ax1.plot(df[100], lw = 3, color = 'red')
+    
+    ax1.set(
+        ylabel = 'Ep (J/kg)', 
+        ylim = [50, 100], 
+        xlabel = 'Years'
         )
     
-    df1 = compute_corr_by_altitude(ds)
+    b.change_axes_color(
+            ax1, 
+            color = 'red',
+            axis = "y", 
+            position = "right"
+            )
+
+
+
+lat_min = -10
+lon_min = -70
+lon_max = -60
+lat_max = 0
+ 
+df = join_all_years(
+        2014, 2017,
+        area = 0,
+        freq = '1M',
+        lon_min = lon_min,
+        lon_max = lon_max,
+        lat_min = lat_min ,
+        lat_max = lat_max,
+        )
+ 
+def plot_timeseries_and_corr_heights():
     
-    plot_altitude_corr(ax, df1, area)
-    
-    ax.set(title= f'Profiles in {lat_min} to {lat_min + 10} by area')
+    return 
+fig, ax = plt.subplots(
+    figsize = (12, 5), 
+    ncols = 2,
+    dpi = 300, 
+    width_ratios= [2, 1]
+    )
+
+plt.subplots_adjust(wspace = 0.5)
+
+
+plot_seasonal_timeseries(ax[0], df)
+
+ds = compute_corr_by_altitude(df)
+
+plot_altitude_corr(ax[1], ds)
+
+title = (f'Latitudes {lat_min}° to {lat_max}° and' + 
+         f'Longitudes {lon_min}° to {lon_max}°')
+
+fig.suptitle(title, )
