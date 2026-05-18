aef21fab52832ee2813bf4d35debea3d51d1d524
LuizFillip <luizfillip@outlook.com>
2026-05-18 11:50:49 -0300

    adding roti contour (year vs. time)

--- ROTI/plot_roti_points_single_like.py
+++ ROTI/plot_roti_points_single_like.py
@@ -4,6 +4,11 @@ import plotting as pl
 import matplotlib.pyplot as plt 
 import base as b 
 import GEO as gg 
+import pandas as pd 
+import numpy as np 
+import matplotlib.dates as mdates
+
+
 def load_and_filter(dn, root = 'D:\\'):
  
     df = pb.get_nighttime_roti(dn, root = root, hours = 10)
@@ -55,22 +60,58 @@ def single_roti(dn):
     
 
 
-dn = dt.datetime(2014, 3, 2, 20)
-# df = load_and_filter(dn, root = 'D:\\')
+def plot_contour_roti(ax, vmax = 5, col_roti = '-50'):
+    df = b.load('database/epbs/roti/maximums_roti2')
+    mask = df.index.month.isin([5, 6, 7, 8])
+  
+    df.loc[mask,  col_roti] *= 0.5 
+    df.loc[df['time'] < 20, 'time'] += 24 
+    
+    df['date'] = pd.to_datetime(df['date'])
+   
+    ds = pd.pivot_table(
+        df,
+        index = 'time', 
+        columns = 'date', 
+        values =  col_roti
+        ) 
+    values =  ds.values
+    
+    values = np.where(values > vmax, vmax, values)
+    
+    img = ax.contourf(
+        ds.columns, 
+        ds.index, 
+        values,
+        levels = np.arange(0,  vmax, 0.2), 
+        cmap = 'turbo'
+        )
+    
+    ax.set(
+        yticks = np.arange(21, 36, 3)[:-1],
+        ylim = [20, 32],
+        yticklabels = ['18', '21', '00', '03'],
+        ylabel = 'Local Time', 
+        xlim = [df['date'].min(), df['date'].max()], 
+        )
+    
+    
+
+    ax.xaxis.set_major_locator(mdates.YearLocator(1))
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y'))
+    
+    ticks = np.arange(0, vmax + 5, 1)
+    
+    cax = ax.inset_axes([1.05, 0., 0.03, 1])
+
+    cb = plt.colorbar(
+        img, 
+        cax = cax,
+        ticks = ticks, 
+        location = 'right'
+        )
+     
+    cb.set_label('ROTI (TECU/min)')
  
- 
 
-fig = single_roti(dn)
-
-# path = 'database/epbs/cg_2009_2024'
-
-# df = b.load(path)
-
-# # df.loc[(df.index.month == dn.month) &
-# #        (df.index.year == dn.year)]
-
-# df.loc[dn.replace(hour = 0)]
- 
-
-
-
+    return None 
